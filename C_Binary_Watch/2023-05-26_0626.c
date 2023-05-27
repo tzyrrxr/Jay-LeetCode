375339652140f41497dbb32642b63c40a921a6c4
tzyrrxr <sjlin0827@outlook.com>
2023-05-27 14:47:17 +0800

    2023-05-27 solve by snprintf allocate size is wrong.

--- C_Binary_Watch/2023-05-26_0626.c
+++ C_Binary_Watch/2023-05-26_0626.c
@@ -1,34 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int H[] = {1, 2, 4, 8};
-int M[] = {1, 2, 4, 8, 16, 32};
-void hr(char **ret, int turnedOn, int *returnSize);
-void min (char **ret, int turnedOn, int *returnSize);
-void hr_min (char **ret, int turnedOn, int *returnSize);
 
 char ** readBinaryWatch(int turnedOn, int* returnSize){
- *returnSize = 0;
- char **ret;
- if (turnedOn > 8) return [];
- ret = (char**) malloc(1000 * (char*));
- // hr
- if (turnedOn < 4) {
-  hr(ret, turnedOn, returnSize);
- }
- // min
- if (turnedOn < 5) {
-  min (ret, turnedOn, returnSize);
- }
- // hr:min
- hr_min (ret, turnedOn, returnSize);
+	char **retArr = (char **) malloc (200*sizeof(char*));
+	int size = 0; // directly use *returnSize, the compiler will consider implicit.
 
- return ret;
+	// half a day == 720 minutes
+	// suppose that the time is defined by minute.
+	// through all conditions.
+	for (int t = 0; t < 720; t++) {
+		int hr = t / 60;
+		int min = t % 60;
+		int LightLed = 0;
+		while (hr) {
+			if (hr % 2 == 1) {
+				LightLed++;
+			}
+			hr = hr >> 1;
+		}
+		while (min) {
+			if (min % 2 == 1) {
+				LightLed++;
+			}
+			min = min >> 1;
+		}
+		// checking
+		//printf("%d\n", LightLed);
+
+		// checking whether LightLed number is equal to turnedOn number.
+		if (LightLed == turnedOn) {
+			// xx:xx + '\0' -> 6 char
+			//
+			retArr[size] = (char*) malloc(6*sizeof(char));
+			snprintf(retArr[size], 6, "%d:%02d", t/60, t%60);
+			//printf("%d:%02d", t/60, t%60);
+			size++;
+		}
+	}
+	*returnSize = size;
+
+	return retArr;	
 }
 
-void hr(ret, turnedOn, returnSize) {
-}
-void min (ret, turnedOn, returnSize) {
-}
-void hr_min (ret, turnedOn, returnSize){
+int main(void) {
+	int size;
+	char **result = readBinaryWatch (1, &size);
+	// checking
+	printf ("Size : %d\n", size);
+	for (int i = 0; i<size; i++)
+		printf("result: %s\n", result[i]);
+	return 0;
 }
