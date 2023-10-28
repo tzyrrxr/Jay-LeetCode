3a72d3dcebb817e233158cce4e2980aeed290e04
tzyrrxr <sjlin0827@outlook.com>
2023-10-28 16:18:31 +0800

    init

--- main.c
+++ main.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int findContentChildren(int* g, int gSize, int* s, int sSize){
  return 0;
 }
 
+
+
+
+
 int main () {
- int *g, *s;
- int gSize, sSize;
  int ret;
- gSize = 4;
- sSize = 4;
+ int g[] = {1, 2, 3};
+ int s[] = {1, 1};
+ int gSize, sSize;
 
+ gSize = sizeof (g) / sizeof (int);
+ sSize = sizeof (s) / sizeof (int);
 
- g = (int *) malloc(gSize * sizeof (int));
- s = (int *) malloc(sSize * sizeof (int));
  ret = findContentChildren(g, gSize, s, sSize);
- printf("%d\n", ret);
+
+ printf("result: %d\n", ret);
 
  return 0;
 }
