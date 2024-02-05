3b8f49f2b77d9ffb92a8362df5d9ffb1b1058a4d
tzyrrxr <sjlin0827@outlook.com>
2024-02-05 22:21:00 +0800

    482

--- easy/482_License_Key_Formatting/main.c
+++ easy/482_License_Key_Formatting/main.c
@@ -5,9 +5,10 @@
 char* licenseKeyFormatting(char* s, int k);
 
 int main() {
- char *s = "5F3Z-2e-9-w";
+ //char *s = "5F3Z-2e-9-w";
+ char *s = "2-5g-3-J";
 
- char *r = licenseKeyFormatting (s, 4);
+ char *r = licenseKeyFormatting (s, 2);
 
  printf("Return: %s\n", r);
 
@@ -21,47 +22,49 @@ char* licenseKeyFormatting(char* s, int k) {
  unsigned int retIndex = 0;
  int  gMember = 0;
  int firstGroupChecker = 0;
+ 
 
  // initialize ret '\0'
  memset (ret, '\0', (10^5)+1);
 
  for (int i = 0; s[i] != '\0'; i++) {
 
-  //if (s[i] == '-') continue;
-
-  // convert lowercases to uppercase
-  // a ~ z
-  // ASCii: 0x61 ~ 0x7A
-  if (s[i] - 0x7B < 0) {
-   s[i] = s[i] - 0x20;
+  // skip '-'
+  if (s[i] == '-' && firstGroupChecker++ > 0) continue;
+  if (firstGroupChecker == 1 && gMember <= k) {
+   if (gMember == k) continue;
+   ret[retIndex++] = '-';
+   gMember = 0;
+   firstGroupChecker++;
+   retIndex++;
+   continue;
   }
 
   // put the char into array
-  if (gMember < k && firstGroupChecker == 0) {
-   ret[retIndex++] = s[i];
+  if (gMember < k && firstGroupChecker != 1) {
+   // convert lowercases to uppercase
+   // a ~ z
+   // ASCii: 0x61 ~ 0x7A
+   if (s[i] < 0x7B && s[i] > 0x60) {
+    ret[retIndex++] = s[i] - 0x20;
+   } else {
+    ret[retIndex++] = s[i];
+   }
    gMember++;
   } else {
    ret[retIndex++] = '-';
+   // convert lowercases to uppercase
+   // a ~ z
+   // ASCii: 0x61 ~ 0x7A
+   if (s[i] < 0x7B && s[i] > 0x60) {
+    ret[retIndex++] = s[i] -0x20;
+   } else {
+    ret[retIndex++] = s[i];
+   }
    gMember = 0;
   }
 
-
-
-
-  // put the character into array.
-
-  //// A ~ Z
-  //// ASCii: 0x41 ~ 0x5A
-  //if (s[i] - 0x5B < 0) {
-  // continue;
-  //}
-
-  // Dash
-  // ASCii: 0x2D
-
-
-
-
  }
+
  return ret;
 }
