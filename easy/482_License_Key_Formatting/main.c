308751f3198de5821a20a1f03800cecd4f372937
tzyrrxr <sjlin0827@outlook.com>
2024-02-06 21:04:49 +0800

    add ignore

--- easy/482_License_Key_Formatting/main.c
+++ easy/482_License_Key_Formatting/main.c
@@ -5,8 +5,8 @@
 char* licenseKeyFormatting(char* s, int k);
 
 int main() {
- //char *s = "5F3Z-2e-9-w";
- char *s = "2-5g-3-J";
+ char *s = "5F3Z-2e-9-w";
+ //char *s = "2-5g-3-J";
 
  char *r = licenseKeyFormatting (s, 2);
 
@@ -21,27 +21,34 @@ char* licenseKeyFormatting(char* s, int k) {
  char *ret = (char *) malloc(sizeof(char)*((10^5)+1));
  unsigned int retIndex = 0;
  int  gMember = 0;
- int firstGroupChecker = 0;
+ int i = 0;
  
 
  // initialize ret '\0'
  memset (ret, '\0', (10^5)+1);
 
- for (int i = 0; s[i] != '\0'; i++) {
+ // first group
+ for (i = 0; s[i] != '-' && gMember < k; i++) {
+   // convert lowercases to uppercase
+   // a ~ z
+   // ASCii: 0x61 ~ 0x7A
+   if (s[i] < 0x7B && s[i] > 0x60) {
+    ret[retIndex++] = s[i] - 0x20;
+   } else {
+    ret[retIndex++] = s[i];
+   }
+   gMember++;
+ }
+ ret[retIndex++] = '-';
+ gMember = 0;
+
+ for (; s[i] != '\0'; i++) {
 
   // skip '-'
-  if (s[i] == '-' && firstGroupChecker++ > 0) continue;
-  if (firstGroupChecker == 1 && gMember <= k) {
-   if (gMember == k) continue;
-   ret[retIndex++] = '-';
-   gMember = 0;
-   firstGroupChecker++;
-   retIndex++;
-   continue;
-  }
+  if (s[i] == '-') continue;
 
   // put the char into array
-  if (gMember < k && firstGroupChecker != 1) {
+  if (gMember < k) {
    // convert lowercases to uppercase
    // a ~ z
    // ASCii: 0x61 ~ 0x7A
