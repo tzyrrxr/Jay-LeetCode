23984d1b18db91e7fe875cb3c12871b7658270ef
tzyrrxr <sjlin0827@outlook.com>
2024-02-07 20:14:18 +0800

    482 finish

--- easy/482_License_Key_Formatting/main.c
+++ easy/482_License_Key_Formatting/main.c
@@ -5,10 +5,11 @@
 char* licenseKeyFormatting(char* s, int k);
 
 int main() {
- char *s = "5F3Z-2e-9-w";
+ //char *s = "5F3Z-2e-9-w";
  //char *s = "2-5g-3-J";
+ char *s = "2-4A0r7-4k";
 
- char *r = licenseKeyFormatting (s, 2);
+ char *r = licenseKeyFormatting (s, 3);
 
  printf("Return: %s\n", r);
 
@@ -16,61 +17,34 @@ int main() {
 }
 
 char* licenseKeyFormatting(char* s, int k) {
- //if (s[0] == '-') return s;
- //char ret[(10^5)+1] = {'\0'}; 
- char *ret = (char *) malloc(sizeof(char)*((10^5)+1));
- unsigned int retIndex = 0;
- int  gMember = 0;
- int i = 0;
- 
 
- // initialize ret '\0'
- memset (ret, '\0', (10^5)+1);
+ // the worst case: a-b-c-d-e-f-...
+ char *ret = (char *) malloc (strlen(s) * 2 * sizeof(char));
+ int group = 0;
+ int i, j;
+ char tmp;
+ memset (ret, '\0', strlen(s)*2);
 
- // first group
- for (i = 0; s[i] != '-' && gMember < k; i++) {
-   // convert lowercases to uppercase
-   // a ~ z
-   // ASCii: 0x61 ~ 0x7A
-   if (s[i] < 0x7B && s[i] > 0x60) {
-    ret[retIndex++] = s[i] - 0x20;
-   } else {
-    ret[retIndex++] = s[i];
-   }
-   gMember++;
- }
- ret[retIndex++] = '-';
- gMember = 0;
-
- for (; s[i] != '\0'; i++) {
-
-  // skip '-'
+ for (i = strlen(s) -1, j = 0 ; i > -1 ; i--) {
   if (s[i] == '-') continue;
-
-  // put the char into array
-  if (gMember < k) {
-   // convert lowercases to uppercase
-   // a ~ z
-   // ASCii: 0x61 ~ 0x7A
-   if (s[i] < 0x7B && s[i] > 0x60) {
-    ret[retIndex++] = s[i] - 0x20;
-   } else {
-    ret[retIndex++] = s[i];
-   }
-   gMember++;
-  } else {
-   ret[retIndex++] = '-';
-   // convert lowercases to uppercase
-   // a ~ z
-   // ASCii: 0x61 ~ 0x7A
-   if (s[i] < 0x7B && s[i] > 0x60) {
-    ret[retIndex++] = s[i] -0x20;
-   } else {
-    ret[retIndex++] = s[i];
-   }
-   gMember = 0;
+  if (group == k) {
+   ret[j++] = '-';
+   group = 0;
   }
+  if (s[i] > 0x60 && s[i] < 0x7B) {
+   ret[j++] = s[i] - 0x20;
+  } else {
+   ret[j++] = s[i];
+  }
+  group++;
+ }
+ //realloc(ret, strlen(ret)+1);
 
+ // reverse string
+ for (i = 0, j = j-1; i < j; i++, j--) {
+  tmp = ret[i];
+  ret[i] = ret[j];
+  ret[j] = tmp;
  }
 
  return ret;
