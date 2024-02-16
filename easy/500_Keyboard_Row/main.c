f4b8e7e33725d9aa3a871a8b1babd25387eb863a
tzyrrxr <sjlin0827@outlook.com>
2024-02-17 07:27:41 +0800

    gitignore: remove *.swo

---
+++ easy/500_Keyboard_Row/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** findWords(char** words, int wordsSize, int* returnSize) {
+    
+}
+
+int main (int argc, char *argv[]) {
+
+ return 0;
+}
