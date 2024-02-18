8505a5dcda15ce0682c271161968217b16b0a703
tzyrrxr <sjlin0827@outlook.com>
2024-02-19 07:58:52 +0800

    501 tmp

--- easy/501_Find_Mode_in_Binary_Search_Tree/main.c
+++ easy/501_Find_Mode_in_Binary_Search_Tree/main.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 
+
+void NextNode(struct TreeNode* root, int* size, int *retArr) {
+  if (!root) return;
+  retArr[(*size)++] = root->val;
+  NextNode (root->left, size, retArr);
+  NextNode (root->right, size, retArr);
+}
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findMode(struct TreeNode* root, int* returnSize) {
+
+  int *retArr = (int *) calloc(10000 + 1, sizeof(int));
+  
+  *returnSize = 0;
+  
+  NextNode(root, returnSize, retArr);
+  
+  //realloc(retArr, (*returnSize) * sizeof(int));
+  
+  return retArr;
+
+}
+
 int main (void)
 
  return 0;
 }
+
