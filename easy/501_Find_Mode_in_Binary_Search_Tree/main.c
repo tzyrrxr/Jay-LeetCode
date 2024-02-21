cb57082c18f22c208da4efdcd2b0d844dc58eb9b
tzyrrxr <sjlin0827@outlook.com>
2024-02-21 22:18:47 +0800

    501 Find Mode in Binary Search Tree

--- easy/501_Find_Mode_in_Binary_Search_Tree/main.c
+++ easy/501_Find_Mode_in_Binary_Search_Tree/main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-
 void NextNode(struct TreeNode* root, int* size, int *retArr) {
   if (!root) return;
-  retArr[(*size)++] = root->val;
+
+  // inorder traversal
   NextNode (root->left, size, retArr);
+  retArr[(*size)++] = root->val;
   NextNode (root->right, size, retArr);
 }
 
@@ -21,7 +22,7 @@ void NextNode(struct TreeNode* root, int* size, int *retArr) {
  */
 int* findMode(struct TreeNode* root, int* returnSize) {
 
-  int *arr = (int *) calloc(10000 + 1, sizeof(int));
+  int *arr = (int *) malloc(10000 * sizeof(int));
   int size = 0;
   int *ret;
   int cur;
@@ -29,31 +30,45 @@ int* findMode(struct TreeNode* root, int* returnSize) {
   int len = 0;
   int index = 0;
 
-  NextNode(root, size, arr);
+
+  NextNode(root, &size, arr);
 
   ret = (int *) malloc(size * sizeof(int));
 
   cur = arr[0];
- for (int i = 0; i < size; i++) {
-  if (cur == arr[i]) {
-   len++;
-  } else {
- 
-  if (len > max) {
-   index = 0;
-   ret[index++] = cur;
-   max = len;
-  } else if (len == max) {
-   ret[i] = cur;
-  }
-   cur = arr[i];
-   len = 1;
-  }
- 
- }
- *returnSize = index;
- return arr;
+  for (int i = 0; i < size; i++) {
+    if (cur == arr[i]) {
+      len++;
+    } else {
 
+      if (len > max) {
+        index = 0;
+        ret[index++] = cur;
+        max = len;
+
+      } else if (len == max) { 
+        ret[index++] = cur;
+
+      }
+        cur = arr[i];
+        len = 1;
+    }
+
+  }
+
+  // if the last number run "if (cur == arr[i])"
+  if (len > max) {
+    index = 0;
+    ret[index++] = cur;
+
+  } else if (len == max) { 
+    ret[index++] = cur;
+
+  }
+  
+
+  *returnSize = index;
+  return ret;
 }
 
 int main (void)
