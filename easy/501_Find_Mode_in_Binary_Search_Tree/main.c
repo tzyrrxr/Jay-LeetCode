de638793ec770000b39142281a4b2973ff7cccd0
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-02-21 20:39:08 +0800

    501 tmp
    501 tmp

--- easy/501_Find_Mode_in_Binary_Search_Tree/main.c
+++ easy/501_Find_Mode_in_Binary_Search_Tree/main.c
@@ -7,6 +7,7 @@ void NextNode(struct TreeNode* root, int* size, int *retArr) {
   NextNode (root->left, size, retArr);
   NextNode (root->right, size, retArr);
 }
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -20,15 +21,38 @@ void NextNode(struct TreeNode* root, int* size, int *retArr) {
  */
 int* findMode(struct TreeNode* root, int* returnSize) {
 
-  int *retArr = (int *) calloc(10000 + 1, sizeof(int));
-  
-  *returnSize = 0;
-  
-  NextNode(root, returnSize, retArr);
-  
-  //realloc(retArr, (*returnSize) * sizeof(int));
-  
-  return retArr;
+  int *arr = (int *) calloc(10000 + 1, sizeof(int));
+  int size = 0;
+  int *ret;
+  int cur;
+  int max = 0;
+  int len = 0;
+  int index = 0;
+
+  NextNode(root, size, arr);
+
+  ret = (int *) malloc(size * sizeof(int));
+
+  cur = arr[0];
+ for (int i = 0; i < size; i++) {
+  if (cur == arr[i]) {
+   len++;
+  } else {
+ 
+  if (len > max) {
+   index = 0;
+   ret[index++] = cur;
+   max = len;
+  } else if (len == max) {
+   ret[i] = cur;
+  }
+   cur = arr[i];
+   len = 1;
+  }
+ 
+ }
+ *returnSize = index;
+ return arr;
 
 }
 
