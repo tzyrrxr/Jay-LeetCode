15c7cabb9092b0d465b91f354dd385784954d4a1
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-02-25 21:06:34 +0800

    n^2 to determine rank

--- easy/506_Relative_Ranks/main.c
+++ easy/506_Relative_Ranks/main.c
@@ -6,42 +6,33 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 char** findRelativeRanks(int* score, int scoreSize, int* returnSize) {
- char **HashTable = (char**) malloc((1000000 + 1) * sizeof(char*));
- char **ret = (char**) malloc(scoreSize * sizeof (char*));
- int order = 0;
+ int rank = 0 ;
+ char** ret = (char **) malloc(scoreSize * sizeof(char*));
  for (int i = 0; i < scoreSize; i++) {
-  HashTable[score[i]] = (char*)calloc('\0', 15 * sizeof(char));
-  HashTable[score[i]] = "1";
- }
-
- order++;
- for (int i = 1000000+1; i > 0; i--, order++) {
-  if (strlen(HashTable[i])) {
-   switch (order) {
-    case 1:
-     strcpy(HashTable[i], "Gold Medal");
-     break;
-    case 2:
-     strcpy(HashTable[i], "Silver Medal");
-     break;
-    case 3:
-     strcpy(HashTable[i], "Bronze Medal");
-     break;
-    default: 
-     sprintf(HashTable[i], "%d", order);
-     break;
-   }
+  rank = 1;
+  for (int j = 0; j < scoreSize; j++) {
+   if (score[i] < score[j]) rank++;
   }
- }
+  ret[i] = (char*) malloc(15 * sizeof(char));
+  switch(rank) {
+   case 1:
+    strcpy(ret[i], "Gold Medal");
+    break;
+   case 2:
+    strcpy(ret[i], "Silver Medal");
+    break;
+   case 3:
+    strcpy(ret[i], "Bronze Medal");
+    break;
+   default:
+    sprintf(ret[i], "%d", rank);
+    break;
+  }
 
- for (int i = 0; i < scoreSize; i++) {
-  ret[i] = (char*) calloc('\0', 15 * sizeof(char));
-  strcpy(ret[i], HashTable[score[i]]);
  }
 
  *returnSize = scoreSize;
  return ret;
-    
 }
 
 
