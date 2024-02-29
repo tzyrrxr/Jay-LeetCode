23ba1887591e108486733e7a3b802dcde95bcd3c
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-01 07:33:18 +0800

    521 Longest Uncommon Subsequence I

--- easy/521_Longest_Uncommon_Subsequence_I/main.c
+++ easy/521_Longest_Uncommon_Subsequence_I/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
 int findLUSlength(char* a, char* b) {
-    
+
+  if (strlen(a) == strlen(b)) {
+    int i;
+    for (i = 0; i < strlen(a); i++) {
+      if (a[i] != b[i]) break;
+    }
+
+    if (i == strlen(a)) return -1;
+  }
+
+  return strlen(a) > strlen(b) ? strlen(a) : strlen(b);
 }
+
 
 int main (void) {
 
