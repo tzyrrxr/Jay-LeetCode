0736081cad40b924b5beedd95e5f7b348f513d71
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-02 10:46:53 +0800

    tmp

---
+++ easy/530_Minimum_Absolute_Difference_In_BST/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int getMinimumDifference(struct TreeNode* root) {
+    
+}
+
+int main (void) {
+
+ return 0;
+}
