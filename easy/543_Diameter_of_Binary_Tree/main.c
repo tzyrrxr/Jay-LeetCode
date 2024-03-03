c2acf49c52d6813f5dab65bb5cd5379397a0d4d6
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-04 07:12:50 +0800

    tmp 543

---
+++ easy/543_Diameter_of_Binary_Tree/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int diameterOfBinaryTree(struct TreeNode* root) {
+    
+}
+
+
+int main (void) {
+
+ return 0;
+}
