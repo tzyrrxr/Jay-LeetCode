ab61c011fcf5296852893d06e778108c84bb946f
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-03 23:37:12 +0800

    541 Reverse String II

--- easy/541_Reverse_String_II/main.c
+++ easy/541_Reverse_String_II/main.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
 
 char* reverseStr(char* s, int k) {
-    
+  if (strlen(s) <= 1) return s;
+  int skip = 0;
+  int probe = 0;
+  int left, right;
+
+  while (probe < strlen(s)) {
+
+    if (!skip) {
+      if (probe + k >= strlen(s)) {
+        k = strlen(s) - probe;
+      }
+
+      char tmp;
+      for (left = probe, right = probe + k -1; left <= right; left++, right--) {
+        tmp = s[right];
+        s[right] = s[left];
+        s[left] = tmp;
+      }
+    } 
+
+    skip = !skip;
+    probe += k;
+
+  }
+
+  return s;
 }
 
 int main (void) {
