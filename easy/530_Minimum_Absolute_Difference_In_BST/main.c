eca814d6e14bbc285415cfa488d4b83bc24dabbe
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-03 17:11:40 +0800

    tmp 530 Minimum Absolute Difference in BST

--- easy/530_Minimum_Absolute_Difference_In_BST/main.c
+++ easy/530_Minimum_Absolute_Difference_In_BST/main.c
@@ -9,7 +9,35 @@
  * };
  */
 int getMinimumDifference(struct TreeNode* root) {
+  if (!root->left && !root->right) return INT_MAX;
+  int retMin = INT_MAX;
+  int leftHistoryMin = INT_MAX;
+  int rightHistoryMin = INT_MAX;
+  int HistoryMin = INT_MAX;
+  int diff = INT_MAX;
+
+  if (root->left) {
+    leftHistoryMin = getMinimumDifference(root->left);
+    diff = root->val - root->left->val;
+    retMin = leftHistoryMin > diff ? diff : leftHistoryMin;
+  }
+  if (root->right) {
+    diff = root->right->val - root->val;
+    retMin = leftHistoryMin > diff ? diff : leftHistoryMin;
+  }
+
+  if (root->right) {
+    rightHistoryMin = getMinimumDifference(root->right);
+    HistoryMin = rightHistoryMin > leftHistoryMin ? leftHistoryMin : rightHistoryMin;
+    diff = root->right->val - root->val;
+    retMin = HistoryMin > diff ? diff : HistoryMin;
+  }
+  if (root->left) {
+    diff = root->val - root->left->val;
+    retMin = HistoryMin > diff ? diff : HistoryMin;
+  }
     
+  return retMin;
 }
 
 int main (void) {
