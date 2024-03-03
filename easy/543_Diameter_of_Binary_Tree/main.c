bf5231e249fb6d8a42ddcd98c2f4aec829d11b5b
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-04 07:44:02 +0800

    543 Diameter of Binary Tree
    Calculating heigh of the subtree

--- easy/543_Diameter_of_Binary_Tree/main.c
+++ easy/543_Diameter_of_Binary_Tree/main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+int inorderTraversal (struct TreeNode *node, int *maxDiameter) {
+  if (!node) {
+    return 0;
+  }
+  int left, right;
+  left = right = 0;
+
+  left = inorderTraversal (node->left, maxDiameter);
+  right = inorderTraversal (node->right, maxDiameter);
+  if (left + right > *maxDiameter) {
+    *maxDiameter = left + right;
+  }
+
+  return left > right ? ++left : ++right;
+
+}
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,6 +25,11 @@
  * };
  */
 int diameterOfBinaryTree(struct TreeNode* root) {
+
+  int diameter = 0;
+  inorderTraversal (root, &diameter);
+
+  return diameter;
     
 }
 
