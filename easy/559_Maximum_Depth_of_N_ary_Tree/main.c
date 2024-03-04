6d5f72eb760c2b18de223b0d574a051b57c3d5d3
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-05 07:40:01 +0800

    tmp 559

---
+++ easy/559_Maximum_Depth_of_N_ary_Tree/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     int numChildren;
+ *     struct Node** children;
+ * };
+ */
+
+int maxDepth(struct Node* root) {
+    
+}
+
+int main (void) {
+
+ return 0;
+}
