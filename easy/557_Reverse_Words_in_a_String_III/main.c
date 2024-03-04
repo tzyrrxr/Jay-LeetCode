956ff404862c2ad86da2ffae0972b80cad4dd80c
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-04 21:23:47 +0800

    fix the last null character

--- easy/557_Reverse_Words_in_a_String_III/main.c
+++ easy/557_Reverse_Words_in_a_String_III/main.c
@@ -3,7 +3,7 @@
 char* reverseWords(char* s) {
   int start, end;
 
-  for (start = 0, end = 0; end < strlen(s); end++) {
+  for (start = 0, end = 0; end < strlen(s) + 1; end++) {
     if (s[end] == ' ' || s[end] == '\0') {
       int l, r, tmp;
       for (l = start, r = end - 1; l < r; l++, r--) {
