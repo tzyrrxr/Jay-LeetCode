c304b8f0b62ccebcc92eff910704037794031efe
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-04 21:21:01 +0800

    tmp terrible solution

---
+++ easy/557_Reverse_Words_in_a_String_III/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+char* reverseWords(char* s) {
+  int start, end;
+
+  for (start = 0, end = 0; end < strlen(s); end++) {
+    if (s[end] == ' ' || s[end] == '\0') {
+      int l, r, tmp;
+      for (l = start, r = end - 1; l < r; l++, r--) {
+        tmp = s[r];
+        s[r] = s[l];
+        s[l] = tmp;
+      }
+      start = end + 1;
+    }
+  }
+
+  return s;
+}
+
+int main (void) {
+
+  return 0;
+}
