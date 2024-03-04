37679a86bf58f94a80b9d668f648c2ec23953bca
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-04 21:30:00 +0800

    terribe solution
    Remark: suggestion to use "while" exception "for"

--- easy/557_Reverse_Words_in_a_String_III/main.c
+++ easy/557_Reverse_Words_in_a_String_III/main.c
@@ -6,10 +6,12 @@ char* reverseWords(char* s) {
   for (start = 0, end = 0; end < strlen(s) + 1; end++) {
     if (s[end] == ' ' || s[end] == '\0') {
       int l, r, tmp;
-      for (l = start, r = end - 1; l < r; l++, r--) {
+      l = start;
+      r = end - 1;
+      while (l < r) {
         tmp = s[r];
-        s[r] = s[l];
-        s[l] = tmp;
+        s[r--] = s[l];
+        s[l++] = tmp;
       }
       start = end + 1;
     }
