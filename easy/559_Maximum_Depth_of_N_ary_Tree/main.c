51b3f342d28f91767e1aced55e9183cd2390a240
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-05 07:52:32 +0800

    tmp 559 Maximum Depth of N-ary Tree

--- easy/559_Maximum_Depth_of_N_ary_Tree/main.c
+++ easy/559_Maximum_Depth_of_N_ary_Tree/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+void traversal(struct Node *root, int level, int *maxLevel) {
+  if (!root) return;
+
+  level++;
+  if (level > *maxLevel) {
+    *maxLevel = level;
+  }
+
+  for (int i = 0; i < root->numChildren; i++) {
+    traversal(root->children[i], level, maxLevel);
+  }
+  
+}
 /**
  * Definition for a Node.
  * struct Node {
@@ -10,6 +23,10 @@
  */
 
 int maxDepth(struct Node* root) {
+  int maxLevel = 0;
+  traversal(root, 0, &maxLevel);
+
+  return maxLevel;
     
 }
 
