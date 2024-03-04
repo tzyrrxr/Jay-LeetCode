39cdd6e17ef6a6368dc981d8f06c5718358c27c8
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-04 20:54:30 +0800

    by switch

--- easy/551_Student_Attendance_Record_I/main.c
+++ easy/551_Student_Attendance_Record_I/main.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
 bool checkRecord(char* s) {
-    
+  int absentTimes = 0;
+  int consecutiveLateTimes = 0;
+  bool ret = true;
+
+  while(*s != '\0') {
+    switch(*s) {
+      case 'A':
+        absentTimes++;
+        consecutiveLateTimes = 0;
+        if (absentTimes > 1) return false;
+        break;
+      case 'L':
+        consecutiveLateTimes++;
+        if (consecutiveLateTimes > 2) return false;
+        break;
+      default: // Present
+        consecutiveLateTimes = 0;
+        break;
+    }
+    s++;
+  }
+
+  return true;
 }
 
 int main (void) {
