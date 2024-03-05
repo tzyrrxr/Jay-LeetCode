426edd45ac76329d063567bb10c060dd781d1a68
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-05 21:28:45 +0800

    bubble sort
    leetcode doesn't accept.
    Big-O: n^2

--- easy/561_Array_Partition/main.c
+++ easy/561_Array_Partition/main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+#define SWAP(a, b) {a = a + b; b = a - b; a = a - b;}
+
 int arrayPairSum(int* nums, int numsSize) {
-    
+  // bubble sort: increasing order
+  int ret = 0;
+  for (int i = 0; i < numsSize; i++) {
+
+    for (int j = 0; j < numsSize-i-1; j++) {
+      if (nums[j] > nums[j+1]) {
+        SWAP(nums[j+1], nums[j])
+      }
+
+    }
+
+    // sum of odd index of number
+    if (i % 2 == 1) {
+      ret += nums[numsSize-i-1];
+    }
+
+  }
+
+  return ret;
 }
 
 int main (void) {
