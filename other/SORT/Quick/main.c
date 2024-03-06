5586a3b2209cd3013e11d7e20de55ee894deb1e1
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-06 22:19:07 +0800

    quick sort ver1

--- other/SORT/Quick/main.c
+++ other/SORT/Quick/main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
 
+void QuickSort(int *arr, int left, int right) {
+  if (left >= right) return;
+
+  int pivotIndex = left;
+  int sortOfLeftIndex = left + 1;
+  int sortOfRightIndex = right;
+
+  for (;sortOfLeftIndex < sortOfRightIndex;) {
+    for (; sortOfLeftIndex <= right && arr[sortOfLeftIndex] < arr[pivotIndex];sortOfLeftIndex++);
+    for (;sortOfRightIndex > left && arr[sortOfRightIndex] > arr[pivotIndex];sortOfRightIndex--);
+    if (sortOfLeftIndex < sortOfRightIndex) {
+      arr[sortOfLeftIndex] = arr[sortOfLeftIndex] + arr[sortOfRightIndex];
+      arr[sortOfRightIndex] = arr[sortOfLeftIndex] - arr[sortOfRightIndex];
+      arr[sortOfLeftIndex] = arr[sortOfLeftIndex] - arr[sortOfRightIndex];
+    }
+  }
+  // change pivot value and meet index
+  arr[left] = arr[left] + arr[sortOfRightIndex];
+  arr[sortOfRightIndex] = arr[left] - arr[sortOfRightIndex];
+  arr[left] = arr[left] - arr[sortOfRightIndex];
+
+  QuickSort (arr, left, sortOfRightIndex-1);
+  QuickSort (arr, sortOfRightIndex+1, right);
+
+}
+
 int main (void) {
 
- return 0;
+  // test
+  int arr[] = {6,2,6,5,1,2};
+  QuickSort (arr, 0, 5);
+
+  return 0;
 }
