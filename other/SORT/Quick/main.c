b397d9291751a3901f9bf7531fadb9abcbf0a888
Sheng-Jie Lin <sjlin0827@outlook.com>
2024-03-09 15:01:42 +0800

    error from swap

--- other/SORT/Quick/main.c
+++ other/SORT/Quick/main.c
@@ -6,21 +6,28 @@ void QuickSort(int *arr, int left, int right) {
   int pivotIndex = left;
   int sortOfLeftIndex = left + 1;
   int sortOfRightIndex = right;
+  int tmp;
 
   for (;sortOfLeftIndex <= sortOfRightIndex;) {
     // finding the exchange index
     for (; sortOfLeftIndex <= right && arr[sortOfLeftIndex] <= arr[pivotIndex];sortOfLeftIndex++);
-    for (;sortOfRightIndex > left+1 && arr[sortOfRightIndex] >= arr[pivotIndex];sortOfRightIndex--);
+    for (;sortOfRightIndex > left && arr[sortOfRightIndex] >= arr[pivotIndex];sortOfRightIndex--);
     if (sortOfLeftIndex <= sortOfRightIndex) {
-      arr[sortOfLeftIndex] = arr[sortOfLeftIndex] + arr[sortOfRightIndex];
-      arr[sortOfRightIndex] = arr[sortOfLeftIndex] - arr[sortOfRightIndex];
-      arr[sortOfLeftIndex] = arr[sortOfLeftIndex] - arr[sortOfRightIndex];
+      //arr[sortOfLeftIndex] = arr[sortOfLeftIndex] + arr[sortOfRightIndex];
+      //arr[sortOfRightIndex] = arr[sortOfLeftIndex] - arr[sortOfRightIndex];
+      //arr[sortOfLeftIndex] = arr[sortOfLeftIndex] - arr[sortOfRightIndex];
+      tmp = arr[sortOfLeftIndex];
+      arr[sortOfLeftIndex] = arr[sortOfRightIndex];
+      arr[sortOfRightIndex] = tmp;
     }
   }
   // exchange pivot value and meet index
-  arr[left] = arr[left] + arr[sortOfRightIndex];
-  arr[sortOfRightIndex] = arr[left] - arr[sortOfRightIndex];
-  arr[left] = arr[left] - arr[sortOfRightIndex];
+  //arr[left] = arr[left] + arr[sortOfRightIndex];
+  //arr[sortOfRightIndex] = arr[left] - arr[sortOfRightIndex];
+  //arr[left] = arr[left] - arr[sortOfRightIndex];
+    tmp = arr[left];
+    arr[left] = arr[sortOfRightIndex];
+    arr[sortOfRightIndex] = tmp;
 
   QuickSort (arr, left, sortOfRightIndex-1);
   QuickSort (arr, sortOfRightIndex+1, right);
@@ -32,6 +39,7 @@ int main (void) {
   // test
   int arr[] = {6,2,6,5,1,2};
   QuickSort (arr, 0, 5);
+  printf("%d\n", arr[0]);
 
   return 0;
 }
