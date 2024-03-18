944841c1af8067ad4e87e1ef698fb539bdb20be2
tzyrrxr <sjlin0827@outlook.com>
2024-03-18 22:10:10 +0800

    599 tmp

---
+++ easy/599_Minimum_Index_Sum_of_Two_Lists/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** findRestaurant(char** list1, int list1Size, char** list2, int list2Size, int* returnSize) {
+    
+}
+
+int main (void) {
+
+ return 0;
+}
