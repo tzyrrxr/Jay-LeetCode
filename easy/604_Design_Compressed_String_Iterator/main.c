2b1234a744fdf4b24d3f90ce956a7441d28c2124
tzyrrxr <sjlin0827@outlook.com>
2024-03-20 07:38:27 +0800

    604 init

---
+++ easy/604_Design_Compressed_String_Iterator/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+typedef struct {
+    
+} StringIterator;
+
+
+StringIterator* stringIteratorCreate(char* compressedString) {
+    
+}
+
+char stringIteratorNext(StringIterator* obj) {
+    
+}
+
+bool stringIteratorHasNext(StringIterator* obj) {
+    
+}
+
+void stringIteratorFree(StringIterator* obj) {
+    
+}
+
+/**
+ * Your StringIterator struct will be instantiated and called as such:
+ * StringIterator* obj = stringIteratorCreate(compressedString);
+ * char param_1 = stringIteratorNext(obj);
+ 
+ * bool param_2 = stringIteratorHasNext(obj);
+ 
+ * stringIteratorFree(obj);
+*/
+
+int main (void) {
+
+ return 0;
+}
