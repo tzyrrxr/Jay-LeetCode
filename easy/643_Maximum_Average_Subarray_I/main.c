5c5475b7325399702124cbf1e44de27bd1765cb4
tzyrrxr <sjlin0827@outlook.com>
2024-03-26 07:35:36 +0800

    643 Maximum Average Subarray I

--- easy/643_Maximum_Average_Subarray_I/main.c
+++ easy/643_Maximum_Average_Subarray_I/main.c
@@ -3,8 +3,18 @@
 double findMaxAverage(int* nums, int numsSize, int k) {
  int max = 0;
  int tmpSum = 0;
+ for (int i = 0; i < k; i++) tmpSum += nums[i];
+ max = tmpSum;
+
+ if (k < numsSize) {
+  for (int i = k; i < numsSize; i++) {
+   tmpSum -= nums[i-k];
+   tmpSum += nums[i];
+   if (max < tmpSum) max = tmpSum;
+  }
+ }
     
- return max;
+ return (double) max / k;
 }
 
 int main (void) {
