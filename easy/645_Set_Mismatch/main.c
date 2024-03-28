4676691242a8c834972b0dbd903f28831e1efb96
tzyrrxr <sjlin0827@outlook.com>
2024-03-28 08:02:04 +0800

    tmp

--- easy/645_Set_Mismatch/main.c
+++ easy/645_Set_Mismatch/main.c
@@ -5,6 +5,7 @@
  */
 // Order is two ways.
 int* findErrorNums(int* nums, int numsSize, int* returnSize) {
+ int min, max;
  // bubble sort
  for (int i = 0; i < numsSize; i++) {
   for (int j = 0; j < numsSize -i -1; j++) {
@@ -16,6 +17,8 @@ int* findErrorNums(int* nums, int numsSize, int* returnSize) {
    }
   }
  }
+ min = nums[0];
+ max = nums[numsSize-1];
 
  int *ret = (int *) malloc(2 * sizeof(int));
 
@@ -28,28 +31,26 @@ int* findErrorNums(int* nums, int numsSize, int* returnSize) {
    ret[1] = nums[1] - 1;
   }
  } else {
-  int order = 0;
-  int left, right;
-  for (int i = 1; i < numsSize && order == 0; i++) {
-   if (i-2 >= 0) left = nums[i-2];
-   else left = nums[i-1];
-   if (i+1 < numsSize) right = nums[i+1];
-   else right = nums[i];
+  // above 3 items
 
-   if (left - right > 0) {
-    // todo
-    // determine order
-   }
-
-   order = nums[i] - nums[i-1];
-  }
+  int tmpIndex;
   for (int i = 1; i < numsSize; i++) {
    if (nums[i-1] == nums[i]) {
     ret[0] = nums[i];
-    ret[1] = nums[i] + order;
+    tmpIndex = i;
     break;
    }
   }
+
+  for (int i = 1; i < numsSize; i++) {
+   if (i == tmpIndex) continue;
+   if (i == numsSize-1) {
+    if (nums[0] == 2) ret[1] = 1;
+    else ret[1] = nums[i] + 1;
+    break;
+   }
+  }
+
  }
 
  *returnSize = 2;
