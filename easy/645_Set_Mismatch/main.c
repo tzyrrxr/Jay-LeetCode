65e144e465e0726666596e9ef4e3c7f3e84ce6ed
tzyrrxr <sjlin0827@outlook.com>
2024-03-29 07:06:36 +0800

    hint: hash table

--- easy/645_Set_Mismatch/main.c
+++ easy/645_Set_Mismatch/main.c
@@ -5,42 +5,43 @@
  */
 // Order is two ways.
 int* findErrorNums(int* nums, int numsSize, int* returnSize) {
- bool hashTable[10000] = {0};
+ bool hashTable[11000] = {0};
 
  int *ret = (int*) malloc(2 * sizeof(int));
+ int min, max;
+ min = INT_MAX;
+ max = INT_MIN;
 
- // bubble sort
- for (int i = 0; i < numsSize; i++) {
-  for (int j = 0; j < numsSize - i - 1; j++) {
-   int tmp;
-   if (nums[j] > nums[j+1]) {
-    tmp = nums[j];
-    nums[j] = nums[j+1];
-    nums[j+1] = tmp;
-   }
-  }
- }
  for (int i = 0; i < numsSize; i++)  {
-  if(hashTable[i]) ret[0] = nums[i];
-  else hashTable[i] = 1;
+  if (min > nums[i]) min = nums[i];
+  if (max < nums[i]) max = nums[i];
+  if(hashTable[nums[i]]) ret[0] = nums[i]; // find the duplicated number
+  else hashTable[nums[i]] = 1;
  }
 
  ret[1] = 0;
- for(int i = nums[1]; i < nums[numsSize-1]; i++) {
+ for(int i = min; i <= max; i++) {
   if (hashTable[i] == 0) {
    ret[1] = i;
    break;
   }
  }
+ 
+ if (!ret[1] && numsSize == 2) {
+  if (ret[0] == 1) ret[1] = 2;
+  else ret[1] = ret[0] - 1;
+ }
 
- if (ret[1] == 0) {
-  if (nums[0] == 1) ret[1] = nums[numsSize-1] + 1; 
-  else ret[1] = nums[0] - 1;
+ if (!ret[1]) {
+  if (min > 1) ret[1] = min - 1;
+  else ret[1] = max + 1;
  }
 
  *returnSize = 2;
  return ret;
 }
+
+
 
 int main (void) {
 
