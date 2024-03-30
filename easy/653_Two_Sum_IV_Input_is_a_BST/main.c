53fdf1bd4249589da2349b140d7b307513f58c5f
tzyrrxr <sjlin0827@outlook.com>
2024-03-31 00:27:52 +0800

    hash table

--- easy/653_Two_Sum_IV_Input_is_a_BST/main.c
+++ easy/653_Two_Sum_IV_Input_is_a_BST/main.c
@@ -1,37 +1,17 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-struct TreeNode {
- int val;
- struct TreeNode *left;
- struct TreeNode *right;
-};
-
-void prefixAddPad (struct TreeNode* node, int k, int *pad, int *padIndex) {
- if (!node) return;
- pad[(*padIndex)++] = k - node->val;
- pad = (int*) realloc(pad, (*padIndex + 1) * sizeof(int));
-
- prefixAddPad (node->left, k, pad, padIndex);
- prefixAddPad (node->right, k, pad, padIndex);
-}
-
-bool infix (struct TreeNode* node, int *pad, int *padSize) {
+bool SearchByHashTable (struct TreeNode* node, int k, bool *padArr) {
  if (!node) return false;
- bool ret = false;
 
- ret = infix (node->left, pad, padSize);
- for (int i = 0; i < *padSize; i++) {
-  if (pad[i] == node->val) {
-   ret = true;
-   break;
-  }
+ if (padArr[node->val + 100000] == 0) {
+  padArr[k - node->val + 100000]++;
+ } else {
+  return true;
  }
- ret = infix (node->right, pad, padSize) || ret;
 
- return ret;
+ return SearchByHashTable(node->left, k, padArr) || SearchByHashTable(node->right, k, padArr);
 }
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -41,21 +21,16 @@ bool infix (struct TreeNode* node, int *pad, int *padSize) {
  * };
  */
 bool findTarget(struct TreeNode* root, int k) {
- int *pad = (int *) malloc(sizeof(int));
- int padIndex = 0;
+ bool *padArr = (bool *) calloc(1000000, sizeof(bool));
  bool ret = false;
 
- prefixAddPad (root, k, pad, &padIndex);
-
- ret = infix (root, pad, &padIndex);
+ ret = SearchByHashTable (root, k, padArr);
 
  return ret;
 }
 
 int main (void) {
 
- int arr[] = [5,3,6,2,4,null,7];
- findTarget (arr);
 
  return 0;
 }
