6cb730f1f225293a6b3eb4ab92f553a52931814e
tzyrrxr <sjlin0827@outlook.com>
2024-04-06 20:33:24 +0800

    1249 Minimum Remove to Make Valid Parentheses

--- medium/1249_Minimum_Remove_to_Make_Valid_Parentheses/main.c
+++ medium/1249_Minimum_Remove_to_Make_Valid_Parentheses/main.c
@@ -4,11 +4,12 @@
 
 char* minRemoveToMakeValid(char* s) {
  int tmp = 0;
- int n = strlen(s);
- int str1Size = 0;
- int str2Size = 0;
- char *str1 = (char*) malloc(sizeof(char));
- char *str2 = (char*) malloc(sizeof(char));
+ unsigned int n = strlen(s);
+ unsigned int str1Size = 0;
+ unsigned int str2Size = 0;
+ // If str1's and str2's address are too close to input the character into str1.
+ char *str1 = (char*) malloc(100000 * sizeof(char));
+ char *str2 = (char*) malloc(100000 * sizeof(char));
 
  // Remove '('
  for (int i = n-1; i >= 0; i--) {
@@ -19,7 +20,7 @@ char* minRemoveToMakeValid(char* s) {
   tmp += ( (s[i] == '(') - (s[i] == ')') );
 
   if (tmp <= 0) {
-   str1 = realloc(str1, (str1Size + 1) * sizeof(char));
+   //str1 = realloc(str1, (str1Size + 1) * sizeof(char));
    str1[str1Size++] = s[i];
   } else {
    tmp = 0; // reset
@@ -35,7 +36,7 @@ char* minRemoveToMakeValid(char* s) {
   tmp += ( (str1[i] == '(') - (str1[i] == ')') );
 
   if (tmp >= 0) {
-   str2 = realloc(str2, (str2Size + 1) * sizeof(char));
+   //str2 = realloc(str2, (str2Size + 1) * sizeof(char));
    str2[str2Size++] = str1[i];
   } else {
    tmp = 0;
