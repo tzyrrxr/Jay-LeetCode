7799987dae26c66b132ea181d9bfe13125901299
tzyrrxr <sjlin0827@outlook.com>
2024-04-07 12:56:32 +0800

    678 Valid Parenthesis String
    Dynamic programming:
    Hint:
    The sum of weight value is the range.

--- medium/678_Valid_Parenthesis_String/main.c
+++ medium/678_Valid_Parenthesis_String/main.c
@@ -1,54 +1,39 @@
 #include <stdio.h>
 
+/*
+ * We can think three characters '(', ')', and '*' as the weighted.
+ * And the sum of the weighted will be the range because '*' can be as increased, decreased, or null.
+ * '(': +1 
+ * ')': -1 // The smallest value of lower bound is 0.
+ * '*': +1, -1, 0
+ * The final value will be the range.
+ * If the value of lower bound is 0, then it is valid parenthesis string.
+ */
 bool checkValidString(char* s) {
- if (strlen(s) <= 1) return false;
- int tmp = 0;
- int i = 0;
- bool ret = false;
+ int lowerBound = 0, higherBound = 0;
+ unsigned int i = 0;
 
- // asterisk as ""
- while (s[i] != '\0') {
-  tmp += ( (s[i] == '(') - (s[i] == ')') );
-
-  if (tmp < 0) {
-   ret = false;
-   break;
-  } 
-
-  i++;
-  ret = true;
- }
- if (ret == 1 && tmp == 0) return true;
-    
- i = 0;
- tmp = 0;
- // asterisk as "("
- while (s[i] != '\0') {
-  tmp += ( (s[i] == '(' || s[i] == '*') - (s[i] == ')') );
-  if (tmp < 0) {
-   ret = false;
-   break;
+ while (s[i]) {
+  if (s[i] == '(') {
+   higherBound++;
+   lowerBound++;
+  } else if (s[i] == ')'){
+   higherBound--;
+   if (--lowerBound < 0) lowerBound = 0;
+  } else if (s[i] == '*') { // increased higher bound, decrease lower bound.
+   higherBound++;
+   if (--lowerBound < 0) lowerBound = 0;
   }
+
+  // too many ')'
+  if (higherBound < 0) return false;
+
   i++;
-  ret = true;
  }
 
- if (ret == 1 && tmp == 0) return true;
-
- i = 0;
- tmp = 0;
- // asterisk as ")"
- while (s[i] != '\0') {
-  tmp += ( (s[i] == '(') - (s[i] == ')' || s[i] == '*') );
-  if (tmp > 0) {
-   ret = false;
-   break;
-  }
-  i++;
-  ret = true;
- }
-
- return (ret == 1 && tmp == 0) ? true : false;
+ // The smallest value of lower bound is 0. 
+ // if too many '(', return false.
+ return lowerBound == 0;
 }
 
 int main (void) {
