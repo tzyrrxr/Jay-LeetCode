411a2f399060a0ce38ec0e41534facbbefbf9be3
tzyrrxr <sjlin0827@outlook.com>
2024-04-10 04:33:30 +0800

    Wonderful solution! Thanks teacher An-Wen Deng.

--- easy/2073_Time_Needed_to_Buy_Tickets/main.c
+++ easy/2073_Time_Needed_to_Buy_Tickets/main.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 
+/*
+ * There are n people to buy tickets.
+ * { tickets = person[Index] | Index = {0 ~ n-1} }
+ * That is person 0 to buy person[0] tickets.
+ * Mark person k to end of time consume.
+ * If Index <= k, then the person <Index> can buy the most to person[k] tickets.
+ * If Index > k, then the person <Index> can buy the most to person[k]-1 tickets.
+ */
+
 int timeRequiredToBuy(int* tickets, int ticketsSize, int k) {
  int sec = 0;
- int i = 0;
+ int pk = tickets[k];
 
- while (1) {
-  if (tickets[i]) {
-   tickets[i]--;
-   sec++;
-  }
-  if (!tickets[k]) break;
-  if (++i >= ticketsSize) i = 0;
+ for (int i = 0; i < ticketsSize; i++) {
+   sec += (i > k) ? 
+   (tickets[i] < pk-1 ? tickets[i] : pk-1) : 
+   (tickets[i] < pk ? tickets[i] : pk);
  }
 
  return sec;
