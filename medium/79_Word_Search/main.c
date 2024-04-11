529e2f85a121ea32284fe15e5bb2f36a381cb31e
tzyrrxr <sjlin0827@outlook.com>
2024-04-11 22:31:10 +0800

    think mouse maze

--- medium/79_Word_Search/main.c
+++ medium/79_Word_Search/main.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
 
-// mouse maze
+struct position {
+ int x;
+ int y;
+} ;
 
+struct position * CreateNode () {
+ struct position *tmp = (struct position*) malloc(sizeof (struct position));
+ return tmp;
+}
+
+// stack problem is like as mouse maze
 bool exist(char** board, int boardSize, int* boardColSize, char* word) {
  int len = strlen(word);
  int top = -1;
- char *stack = (char*) malloc(len);
+ struct position *posStack = (struct position*) malloc(len * sizeof (struct position));
+
  for (int i = 0; i < boardSize; i++) {
   for (int j = 0; j < boardColSize; j++) {
-   while (top != -1)
+   if (*word == board[i][j]) {
+    // mark first character
+    posStack[++top] = CreateNode ();
+    posStack[++top]->x = i;
+    posStack[++top]->y = j;
+
+    // right
+    // down
+    // left
+    // top
+    
+   }
   }
  }
     
