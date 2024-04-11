963cab03ca29835cb798777b947a916928e820f9
tzyrrxr <sjlin0827@outlook.com>
2024-04-11 22:17:30 +0800

    tmp

---
+++ medium/79_Word_Search/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// mouse maze
+
+bool exist(char** board, int boardSize, int* boardColSize, char* word) {
+ int len = strlen(word);
+ int top = -1;
+ char *stack = (char*) malloc(len);
+ for (int i = 0; i < boardSize; i++) {
+  for (int j = 0; j < boardColSize; j++) {
+   while (top != -1)
+  }
+ }
+    
+}
+
+int main (void) {
+
+ return 0;
+}
