56d2ef7a77a57991bfa0d57e40c1efae0bbbbdc3
tzyrrxr <sjlin0827@outlook.com>
2024-04-11 21:56:28 +0800

    1614 Maximum Nesting Depth of the Parenthese
    Count the '('.

--- easy/1614_Maximum_Nesting_Depth_of_the_Parentheses/main.c
+++ easy/1614_Maximum_Nesting_Depth_of_the_Parentheses/main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
 int maxDepth(char* s) {
-    
+ int max = 0;
+ int len = strlen(s);
+ int buffer = 0;
+
+ for (int i = 0; i < len; i++) {
+  buffer += (s[i] == '(') - (s[i] == ')');
+  if (buffer > max) max = buffer;
+ }
+
+ return max;
 }
 
 int main (void) {
