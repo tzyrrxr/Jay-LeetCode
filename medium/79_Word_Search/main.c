d5012b506cf237652cb55031cd8bda588946a586
tzyrrxr <sjlin0827@outlook.com>
2024-04-14 14:41:56 +0800

    79 Word Search
    Remark: It is careful that the default size is whether is a pointer or
    the number.

--- medium/79_Word_Search/main.c
+++ medium/79_Word_Search/main.c
@@ -1,33 +1,34 @@
 #include <stdio.h>
 
 
-bool DFS(char** board, int x, int y, char *w, int wP) {
+bool DFS(char** board, int bW, int bH, int x, int y, char *w, int wP) {
  int len = strlen(w);
  if (wP >= len) return true;
 
  // define direction
- int dx[] = {0, 1,  0, -1}
+ int dx[] = {0, 1,  0, -1};
  int dy[] = {1, 0, -1,  0};
 
  char tmp;
- bool ret = flase;
+ bool ret = false;
 
  for (int d = 0; d < 4; d++) {
   // checking the wall
-  if () {
-  }
+  if (x + dx[d] >= bW || x + dx[d]  < 0 ||
+      y + dy[d] >= bH || y + dy[d] < 0 
+    ) continue;
 
   // checking character
-  if (w[wP] == board[x][y]) {
-   tmp = board[x][y];
-   board[x][y] = '-';
-   ret = true && DFS (board, x+dx[d], y+dy[d], w, wP+1);
-   board[x][y] = tmp;
+  if (w[wP] == board[y + dy[d]][x + dx[d]]) {
+   tmp = board[y + dy[d]][x + dx[d]];
+   board[y + dy[d]][x + dx[d]] = '-';
+   ret = true && DFS (board, bW, bH, x+dx[d], y+dy[d], w, wP+1);
+   board[y + dy[d]][x + dx[d]] = tmp;
+   if (ret) return ret;
   }
  }
 
  return ret;
-
 }
 // stack problem 
 // similar mouse maze 
@@ -36,12 +37,16 @@ bool exist(char** board, int boardSize, int* boardColSize, char* word) {
  int wP = 0; // word pointer
  int top = -1;
  bool ret = false;
+ char tmp;
 
- for (int i = 0; i < boardSize; i++) {
-  for (int j = 0; j < boardColSize; j++) {
-   if (board[i][j] == *word) {
+ for (int y = 0; y < boardSize; y++) {
+  for (int x = 0; x < *boardColSize; x++) {
+   if (board[y][x] == *word) {
     // Depth First Search
-    ret = DFS (board, i, j, word, wP+1);
+    tmp = board[y][x];
+    board[y][x] = '-';
+    ret = DFS (board, *boardColSize, boardSize, x, y, word, wP+1);
+    board[y][x] = tmp;
 
     if (ret) return ret; // Exist
    }
