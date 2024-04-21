2e4b59add02c15512291a6caf07c022a6c18ef4d
tzyrrxr <sjlin0827@outlook.com>
2024-04-21 20:58:08 +0800

    still memory limit exceeded

--- easy/1971_Find_if_Path_Exists_in_Graph/main.c
+++ easy/1971_Find_if_Path_Exists_in_Graph/main.c
@@ -21,24 +21,28 @@ bool DFS (int **p, int s, int d, bool *visited) {
 
 bool validPath(int n, int** edges, int edgesSize, int* edgesColSize, int source, int destination) {
  if (n == 1) return true;
- int m = 0;
- if (n / edgesSize > 10000) m = 100;
- else if (n >= 20000) m = 300;
- else if (n > 10000) m = 500;
- else m = 800;
+ //int m = 0;
+ //if (n / edgesSize > 10000) m = 100;
+ //else if (n >= 20000) m = 300;
+ //else if (n > 10000) m = 500;
+ //else m = 800;
 
  int **path;
  int ret = false;
  int u, v;
  int *visited;
  path = (int**) calloc(n, sizeof(int*));
- for (int i = 0; i < n; i++) path[i] = (int*) calloc(m, sizeof(int));
+ for (int i = 0; i < n; i++) path[i] = NULL;
+  
 
 
  // record edges
  for (int i = 0; i < edgesSize; i++) {
   u = edges[i][0];
   v = edges[i][1];
+
+  if (!path[u]) path[u] = (int*) calloc(edgesSize+1, sizeof(int));
+  if (!path[v]) path[v] = (int*) calloc(edgesSize+1, sizeof(int));
 
   path[u][0]++;
   path[u][path[u][0]] = v;
@@ -52,6 +56,7 @@ bool validPath(int n, int** edges, int edgesSize, int* edgesColSize, int source,
     
  return ret;
 }
+
 int main (void) {
 
  return 0;
