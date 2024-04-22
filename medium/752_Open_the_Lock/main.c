8b551823ac8204e34dffc0ce09da30b96bcd1f9e
tzyrrxr <sjlin0827@outlook.com>
2024-04-23 06:50:03 +0800

    752 Open the Lock

--- medium/752_Open_the_Lock/main.c
+++ medium/752_Open_the_Lock/main.c
@@ -2,35 +2,47 @@
 #include <stdlib.h>
 
 int openLock(char** deadends, int deadendsSize, char* target) {
- int dead[10000] = {0};
- int visited[10000] = {0};
- int tmp;
+ if (atoi(target) == 0) return 0;
+ bool dead[10000] = {0};
+ bool visited[10000] = {0};
  int steps = 0;
+ char curr[5];
+ char next[5];
 
- char stack[10000][5];
- int top = -1;
+ char queue[20000][5] = {0};
+ int front, rear;
+ front = rear = -1;
 
  for (int i = 0; i < deadendsSize; i++) {
-  tmp = atoi(deadends[i]);
-  if (!tmp) return -1;
+  int tmp = atoi(deadends[i]);
+  if (tmp == 0) return -1;
   else dead[tmp] = 1;
  }
 
- stack[++top] = "0000\0";
+ strncpy(queue[++rear], "0000", 4);
+ queue[rear][4] = '\0';
+ front = 0;
 
- while (top != -1) {
+ while (front <= rear) {
   steps++;
-  int size = top + 1;
-  for (int i = 0; i < size; i++) {
-   char* curr = stack[top];
-   top--;
+  int edge = rear + 1;
+  for (int i = front; i < edge; i++) {
+   strncpy(curr, queue[i], 4);
+   curr[4] = '\0';
+   front++;
+
    for (int j = 0; j < 4; j++) {
-    for (int z = -1; z <=1; z+=2) {
-     curr[j] = (curr[j] - '0' + z + 10) % 10 + '0';
-     if (strcmp(curr) == strcmp(target)) return steps;
-     if (dead[atoi(curr)] || visited[atoi(curr)]) continue;
-     stack[++top] = curr;
-     visited[atoi(curr)] = 1;
+    for (int z = -1; z <= 1; z+=2) {
+     strncpy(next, curr, 4);
+     next[4] = '\0';
+
+     next[j] = (next[j] - '0' + z + 10) % 10 + '0';
+     if (!strcmp(next, target)) return steps;
+     if (dead[atoi(next)] || visited[atoi(next)]) continue;
+     strncpy(queue[++rear], next, 4);
+     queue[rear][4] = '\0';
+     visited[atoi(next)] = 1;
+
     }
    }
   }
