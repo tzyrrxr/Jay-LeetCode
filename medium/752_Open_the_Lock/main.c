0f340db08a02ca284535ada0a8a008aec7d4930d
tzyrrxr <sjlin0827@outlook.com>
2024-04-22 22:00:30 +0800

    tmp

--- medium/752_Open_the_Lock/main.c
+++ medium/752_Open_the_Lock/main.c
@@ -3,10 +3,11 @@
 
 int openLock(char** deadends, int deadendsSize, char* target) {
  int dead[10000] = {0};
+ int visited[10000] = {0};
  int tmp;
  int steps = 0;
 
- char stack[10000][4];
+ char stack[10000][5];
  int top = -1;
 
  for (int i = 0; i < deadendsSize; i++) {
@@ -15,9 +16,25 @@ int openLock(char** deadends, int deadendsSize, char* target) {
   else dead[tmp] = 1;
  }
 
- stack[++top] = "0000";
+ stack[++top] = "0000\0";
 
-
+ while (top != -1) {
+  steps++;
+  int size = top + 1;
+  for (int i = 0; i < size; i++) {
+   char* curr = stack[top];
+   top--;
+   for (int j = 0; j < 4; j++) {
+    for (int z = -1; z <=1; z+=2) {
+     curr[j] = (curr[j] - '0' + z + 10) % 10 + '0';
+     if (strcmp(curr) == strcmp(target)) return steps;
+     if (dead[atoi(curr)] || visited[atoi(curr)]) continue;
+     stack[++top] = curr;
+     visited[atoi(curr)] = 1;
+    }
+   }
+  }
+ }
 
  return -1;
 }
