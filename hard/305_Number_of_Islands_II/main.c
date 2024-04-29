aa023cae9ba09c36d187153470c44075b5b0a772
tzyrrxr <sjlin0827@outlook.com>
2024-04-29 08:02:06 +0800

    init

---
+++ hard/305_Number_of_Islands_II/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* numIslands2(int m, int n, int** positions, int positionsSize, int* positionsColSize, int* returnSize) {
+    
+}
+
+int main (void) {
+
+ return 0;
+}
