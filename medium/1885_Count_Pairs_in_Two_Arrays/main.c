d635d7e95a9adc4967f97a3524ab8d86d143d9ce
tzyrrxr <sjlin0827@outlook.com>
2024-05-03 21:05:29 +0800

    tmp

--- medium/1885_Count_Pairs_in_Two_Arrays/main.c
+++ medium/1885_Count_Pairs_in_Two_Arrays/main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 
-// i < j
-// nums1[i] + nums1[j] > nums2[i] + nums2[j]
-// nums1[i] - nums2[i] > num2[j] - nums1[j]
+/*
+ * i < j
+ * nums1[i] + nums1[j] > nums2[i] + nums2[j]
+ * nums1[i] - nums2[i] > num2[j] - nums1[j]
+ * (nums1[i] - nums2[i]) - (num2[j] - nums1[j]) > 0
+ * (nums1[i] - nums2[i]) + (num1[j] - nums2[j]) > 0
+*/
 
 long long countPairs(int* nums1, int nums1Size, int* nums2, int nums2Size) {
  long long ret = 0;
  int l, r;
  int len = nums1Size;
+ int diff[len];
  
- for (int i = 0; i < len-1; i++) {
-  for (int j = i+1; j < len; j++) {
-   l = nums1[i] - nums2[i];
-   r = nums2[j] - nums1[j];
-   if (l > r) ret++;
-  }
+ for (int z = 0; z < len; z++) {
+  diff[z] = nums1[z] - nums2[z];
  }
 
  return ret;
