7a73ce8824ac57bf124ac8bbb3458c5192dbcf6d
tzyrrxr <sjlin0827@outlook.com>
2024-05-03 21:21:38 +0800

    tmp

---
+++ medium/165_Compare_Version_Numbers/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int compareVersion(char* version1, char* version2) {
+ int ret;
+ int ver1, ver2;
+ int i;
+ char *tmp = (char*) calloc(500, sizeof(char));
+
+ i = 0;
+ while (version1[i] != '\0') {
+  if (version1[i] == '.') {
+  }
+ }
+
+ return ret;
+}
+
+int main (void) {
+
+ return 0;
+}
