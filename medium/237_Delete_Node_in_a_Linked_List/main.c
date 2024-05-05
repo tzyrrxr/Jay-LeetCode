6b517731992f913f01ec28ad3ca61efcf407f217
tzyrrxr <sjlin0827@outlook.com>
2024-05-05 09:31:04 +0800

    237 Delete Node in a Linked List

---
+++ medium/237_Delete_Node_in_a_Linked_List/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node) {
+ struct ListNode* tmp = node->next;
+
+ *node = *node->next;
+
+ free(tmp);
+}
+
+int main (void) {
+
+ return 0;
+}
