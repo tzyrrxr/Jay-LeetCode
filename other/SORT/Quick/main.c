351f1f364f94241031c9f0e705628d369f0188a0
tzyrrxr <sjlin0827@outlook.com>
2024-05-05 14:33:03 +0800

    rewrite quick sort

--- other/SORT/Quick/main.c
+++ other/SORT/Quick/main.c
@@ -1,6 +1,64 @@
 #include <stdio.h>
 
-void QuickSort(int *arr, int left, int right) {
+void QS_inc (int *pt, int l, int r) {
+ if (l >= r) return;
+
+ int pivot = l;
+ int lI = l + 1;
+ int rI = r;
+ int tmp;
+
+ while (lI <= rI) {
+  // search target
+  while(lI <= r && pt[lI] <= pt[pivot]) lI++;
+  while(l < rI && pt[pivot] <= pt[rI]) rI--;
+  if (lI < rI) {
+   tmp = pt[lI];
+   pt[lI] = pt[rI];
+   pt[rI] = tmp;
+  }
+  // exchange the pivot into the meet between left and right index
+  tmp = pt[pivot];
+  pt[pivot] = pt[rI];
+  pt[rI] = tmp;
+
+ }
+
+ QS_inc (pt, l, rI-1);
+ QS_inc (pt, rI+1, r);
+
+}
+
+void QS_dec (int *pt, int l, int r) {
+ if (l >= r) return;
+
+ int pivot = l;
+ int lI = l + 1;
+ int rI = r;
+ int tmp;
+
+ while (lI <= rI) {
+  // search target
+  while(lI <= r && pt[pivot] <= pt[lI]) lI++;
+  while(l < rI && pt[rI] <= pt[pivot]) rI--;
+  if (lI < rI) {
+   tmp = pt[lI];
+   pt[lI] = pt[rI];
+   pt[rI] = tmp;
+  }
+  // exchange the pivot into the meet between left and right index
+  tmp = pt[pivot];
+  pt[pivot] = pt[rI];
+  pt[rI] = tmp;
+
+ }
+
+ QS_dec (pt, l, rI-1);
+ QS_dec (pt, rI+1, r);
+
+}
+
+void QuickSort_V1(int *arr, int left, int right) {
   if (left >= right) return;
 
   int pivotIndex = left;
@@ -29,8 +87,8 @@ void QuickSort(int *arr, int left, int right) {
     arr[left] = arr[sortOfRightIndex];
     arr[sortOfRightIndex] = tmp;
 
-  QuickSort (arr, left, sortOfRightIndex-1);
-  QuickSort (arr, sortOfRightIndex+1, right);
+  QuickSort_V1 (arr, left, sortOfRightIndex-1);
+  QuickSort_V1 (arr, sortOfRightIndex+1, right);
 
 }
 
@@ -38,8 +96,10 @@ int main (void) {
 
   // test
   int arr[] = {6,2,6,5,1,2};
-  QuickSort (arr, 0, 5);
-  printf("%d\n", arr[0]);
+  //QuickSort_V1 (arr, 0, 5);
+  //QS_inc (arr, 0, 5);
+  QS_dec (arr, 0, 5);
+  puts("done");
 
   return 0;
 }
