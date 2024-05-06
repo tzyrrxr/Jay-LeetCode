2dde7aac689aeb357724851e3cf804de8207d537
tzyrrxr <sjlin0827@outlook.com>
2024-05-06 21:30:12 +0800

    2487 Remove Nodes From Linked List
    Stack

---
+++ medium/2487_Remove_Nodes_From_Linked_List/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* removeNodes(struct ListNode* head) {
+ struct ListNode *ret;
+ struct ListNode *pt;
+ struct ListNode **s = (struct ListNode **) malloc(100001 * sizeof(struct ListNode*));
+ int top = -1;
+
+ pt = head;
+ while (pt) {
+
+  if (top == -1) {
+   s[++top] = pt;
+   pt = pt->next;
+   continue;
+  } 
+
+  while (top > -1 && s[top]->val < pt->val) {
+   top--;
+  }
+
+  s[++top] = pt;
+  pt = pt->next;
+
+ }
+
+ ret = s[0];
+ for (int i = 0; i < top; i++) {
+  s[i]->next = s[i+1];
+ }
+
+ return ret;
+}
+
+int main (void) {
+
+ return 0;
+}
