9eb59450a56a1a1173b0820de1a694c1e0fa28ff
tzyrrxr <sjlin0827@outlook.com>
2024-05-20 07:37:43 +0800

    165 Compare Version Numbers

--- medium/165_Compare_Version_Numbers/main.c
+++ medium/165_Compare_Version_Numbers/main.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int compareVersion(char* version1, char* version2) {
- int ret;
- int ver1, ver2;
- int i;
- char *tmp = (char*) calloc(500, sizeof(char));
 
- i = 0;
- while (version1[i] != '\0') {
-  if (version1[i] == '.') {
-  }
+ while (*version1 || *version2) {
+  long long v1 = strtol (version1, &version1, 10);
+  long long v2 = strtol (version2, &version2, 10);
+  if (v1 < v2) return -1;
+  if (v1 > v2) return 1;
+
+  if (*version1) version1++;
+  if (*version2) version2++;
  }
 
- return ret;
+ return 0;
+
 }
 
 int main (void) {
+ char *str = "123.456.789";
+ while (*str) {
+  printf("%ld\n", strtol(str, &str, 10));
+  printf("%c\n", *str);
+
+  puts("Done");
+  if (*str) str++;
+ }
+
+ // OUTPUT:
+ //  123
+ //  .
+ //  Done
+ //  456
+ //  .
+ //  Done
+ //  789
+ //  
+ //  Done
 
  return 0;
 }
