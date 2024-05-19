cdbbaa3fb6b39224b0da526a03a85c6642c00fbe
tzyrrxr <sjlin0827@outlook.com>
2024-05-19 18:04:41 +0800

    3068 Find the Maximum Sum of Node Values

--- hard/3068_Find_the_Maximum_Sum_of_Node_Values/main.c
+++ hard/3068_Find_the_Maximum_Sum_of_Node_Values/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <limits.h>
 
-// a XOR b XOR b == a --> Item b is the even item
+// a XOR b XOR b == a
 // a XOR a == 0
+// isEven : 0 == odd, 1 == even
 long long maxSumOfNodes (int *nums, int numsSize, int k, int index, int isEven, long long **memo) {
  if (index == numsSize) {
   return isEven == 1 ? 0 : INT_MIN;
@@ -11,22 +12,32 @@ long long maxSumOfNodes (int *nums, int numsSize, int k, int index, int isEven, 
  // if the item is memoried, return its value
  if (memo[index][isEven] != -1) return memo[index][isEven];
 
+ // noXORIndex == this item do not performs XOR operation.
+ // XORIndex == this item performs an XOR operation.
  long long noXORIndex = nums[index] + maxSumOfNodes (nums, numsSize, k, index+1, isEven, memo);
- long long XORIndex = nums[index]^k + maxSumOfNodes (nums, numsSize, k, index+1, isEven^1, memo); // 0^1 == 1, 1^1 == 0
+ long long XORIndex = (nums[index]^k) + maxSumOfNodes (nums, numsSize, k, index+1, isEven^1, memo); // 0^1 == 1, 1^1 == 0
 
  // memoried the item
  return memo[index][isEven] = noXORIndex < XORIndex ? XORIndex : noXORIndex;
 }
 
 long long maximumValueSum(int* nums, int numsSize, int k, int** edges, int edgesSize, int* edgesColSize) {
+
  long long **memo = (long long **) malloc(numsSize * sizeof(long long*));
+ long long ret;
+
  for (int i = 0; i < numsSize; i++) {
   memo[i] = (long long *) malloc(2 * sizeof(long long));
   memo[i][0] = -1;
   memo[i][1] = -1;
  }
 
- return maxSumOfNodes(nums, numsSize, k, 0, 1, memo);
+ ret = maxSumOfNodes(nums, numsSize, k, 0, 1, memo);
+
+ for (int i = 0; i < numsSize; i++) free(memo[i]);
+ free(memo);
+
+ return ret;
     
 }
 
