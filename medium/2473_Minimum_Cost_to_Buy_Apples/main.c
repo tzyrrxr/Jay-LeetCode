a28ad52f8350b888a7da0db003fa0c76ff19d79a
tzyrrxr <sjlin0827@outlook.com>
2024-05-22 07:39:16 +0800

    2473 init

---
+++ medium/2473_Minimum_Cost_to_Buy_Apples/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+long long* minCost(int n, int** roads, int roadsSize, int* roadsColSize, int* appleCost, int appleCostSize, int k, int* returnSize) {
+    
+}
+
+int main (void) {
+
+ return 0;
+}
