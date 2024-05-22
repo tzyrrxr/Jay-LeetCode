c6fbf4a6319922c4c540e5af4e3a35e1a1a4f720
tzyrrxr <sjlin0827@outlook.com>
2024-05-22 22:16:04 +0800

    tmp test

---
+++ medium/131_Palindrome_Partitioning/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+bool isPalindrome (char *s) {
+
+ int len = strlen(s) / 2;
+
+ for (int i = 0; i < len; i++) if (s[i] != s[len-1-i]) return false;
+
+ return true;
+
+}
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+char*** partition(char* s, int* returnSize, int** returnColumnSizes) {
+ char ***ret;
+    
+ return ret;
+
+}
+
+int main (void) {
+ 
+ return 0;
+}
