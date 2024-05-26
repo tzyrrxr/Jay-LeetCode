427d8a8f9452db2b9805e197d03031d75567b46a
tzyrrxr <sjlin0827@outlook.com>
2024-05-27 07:19:09 +0800

    tmp

--- medium/17_Letter_Combinations_of_a_Phone_Number/main.c
+++ medium/17_Letter_Combinations_of_a_Phone_Number/main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
+void BT (int index, char* s, char* digits, char **ret, int *retSize) {
+ if (index == strlen(digits)) {
+  (*ret)[++(*retSize)] = (char*) malloc(index, sizeof(char));
+  strncpy((*ret)[(*retSize)], s, index);
+  (*ret)[(*retSize)][index] = '\0';
+  return;
+ } 
+}
+
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 char** letterCombinations(char* digits, int* returnSize) {
+ char **ret;
+ int index = 0;
+
+ BT (index, digits, ret, returnSize);
     
+ return ret;
 }
 
 int main (void) {
