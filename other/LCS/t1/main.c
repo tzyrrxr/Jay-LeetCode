24f1e661acadae28e5ca41f2e4472ef2e9cda276
tzyrrxr <sjlin0827@outlook.com>
2025-03-16 09:19:24 +0800

    LCS by memset

--- other/LCS/t1/main.c
+++ other/LCS/t1/main.c
@@ -3,39 +3,24 @@
 #include <stdlib.h>
 
 int longestCommonSubsequence (char* text1, char* text2) {
-  int **table;
   int len1 = strlen(text1);
   int len2 = strlen(text2);
-  int max = 0;
+  int table[len1+1][len2+2];
+  memset(table, 0, sizeof(table));
 
-  table = (int**) malloc (len1 * sizeof(int*));
-  for (int i = 0; i < len1; i++) table[i] = (int*) calloc (len2, sizeof(int));
-
-  for (int i = 0; i < len1; i++) {
-    for (int j = 0; j < len2; j++) {
-      if (text1[i] == text2[j]) {
-
-        if (i - 1 < 0 || j - 1 < 0) {
-          table[i][j] = 1;
-        } else {
-          table[i][j] = table[i-1][j-1] + 1;
-        }
-
+  for (int i = 1; i < len1 + 1; i++) {
+    for (int j = 1; j < len2 + 1; j++) {
+      if (text1[i-1] == text2[j-1]) {
+        table[i][j] = table[i-1][j-1] + 1;
       } else {
-        if (i - 1 < 0 && j - 1 < 0) {
-          table[i][j] = 0;
-        } else if (i - 1 < 0) {
-          table[i][j] = 0 > table[i][j-1] ? 0 : table[i][j-1];
-        } else if (j - 1 < 0) {
-          table[i][j] = table[i-1][j] > 0 ? table[i-1][j] : 0;
-        } else {
-          table[i][j] = table[i-1][j] > table[i][j-1] ? table[i-1][j] : table[i][j-1];
-        }
+        table[i][j] = table[i-1][j] > table[i][j-1] ? table[i-1][j] : table[i][j-1];
       }
     }
+
   }
 
-  return table[len1 - 1][len2 - 1];
+
+  return table[len1][len2];
 
 }
 
