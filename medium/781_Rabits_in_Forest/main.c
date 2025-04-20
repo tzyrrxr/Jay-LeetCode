03a7276b79fa7404794cb85be10d17432381516c
tzyrrxr <sjlin0827@outlook.com>
2025-04-20 10:46:51 +0800

    781 at least count

---
+++ medium/781_Rabits_in_Forest/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int numRabbits(int* answers, int answersSize) {
+  // array answers is meaning visible rabbits
+  // array answers's value means that how many habbits as same as my color except myself
+  int atLastRabbitsNum = 0;
+  int call[1000] = {0};
+
+  for (int i = 0; i < answersSize; i++) {
+    call[answers[i]]++;
+  }
+
+  for (int i = 0; i < 1000; i++) {
+    atLastRabbitsNum += (((-1*call[i]) % (i+1)) + i+1) % (i+1)// need to complete this groups 
+     + call[i]; // at least number of rabbits
+  }
+    
+  return atLastRabbitsNum;
+
+}
+
+int main (void) {
+ 
+ return 0;
+}
