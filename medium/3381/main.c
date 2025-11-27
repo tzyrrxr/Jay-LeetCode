ca69ecd34e23227dcb943fb636671727463a9517
tzyrrxr <sjlin0827@outlook.com>
2025-11-27 19:10:21 +0800

    3381 presum, remainder

--- medium/3381/main.c
+++ medium/3381/main.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
+// presum
+// remainder
 long long maxSubarraySum(int* nums, int numsSize, int k) {
-  long long max = (long long)INT_MIN;
-  long long sum = (long long)INT_MIN;
-  for (int z = 1; z*k <= numsSize; z++) {
-    // i contains first index of subarray
-    for (int i = 0; i < numsSize - k*z + 1; i++) {
-      sum = 0;
-      for (int j = 0; j < k*z; j++) {
-        sum += nums[i +j];
-      }
-      if (sum > max) max = sum;
-    } 
+  long long max = LLONG_MIN;
+  long long minPre = LLONG_MAX;
+  long long *presum = (long long *) malloc ((numsSize+1) * sizeof (long long));
+  //int sublen = 0;
+
+  presum[0] = 0;
+  for (int i = 0; i < numsSize; i++) {
+    presum[i+1] = presum[i] + nums[i];
   }
+
+  // remainder
+  for (int r =0; r < k; r++) { 
+    // reset minPre for each remainder
+    minPre = LLONG_MAX;
+    for (int i = r; i <= numsSize; i+=k) {
+      if (minPre != LLONG_MAX) {
+        if (max < presum[i] - minPre) {
+          max = presum[i] - minPre; // presum[i]->more; minPre->less ==> max
+        }
+
+      }
+      if (presum[i] < minPre) {
+        minPre = presum[i];
+      }
+    }
+  }
+
+  free(presum);
 
   return max;
 }
