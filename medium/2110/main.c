1534d1907cf1d9b866a99ea9465e1b7b4a0c0ebf
tzyrrxr <sjlin0827@outlook.com>
2025-12-15 14:15:51 +0800

    2110 compute the summation simultaneously

--- medium/2110/main.c
+++ medium/2110/main.c
@@ -1,30 +1,17 @@
 long long getDescentPeriods(int* prices, int pricesSize) {
-  long long cnt = 0; 
-  long long *stack = (long long*) malloc(pricesSize * sizeof(long long));
-  int top = -1;
+  long long cnt = 1; 
   int smoothDes = 1;
 
   // counting as long as possible
-  for (int i = 0; i < pricesSize-1; i++) {
-    if (prices[i] - 1 == prices[i+1]) {
+  // compute the summation simultaneously
+  for (int i = 1; i < pricesSize; i++) {
+    if (prices[i-1] - 1 == prices[i]) {
       smoothDes++;
     } else {
-      stack[++top] = smoothDes;
       smoothDes = 1;
     }
+    cnt += smoothDes;
   }
-
-  stack[++top] = smoothDes; // the last one
-
-  // summation
-  while (top >= 0) {
-    for (int i = 1; i <= stack[top]; i++) {
-      cnt = cnt + i;
-    }
-    top--;
-  }
-  
-  free(stack);
          
   return cnt;
 
