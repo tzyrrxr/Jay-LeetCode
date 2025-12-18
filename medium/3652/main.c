659690517aef25a65fe5c4e16323301addbde15f
tzyrrxr <sjlin0827@outlook.com>
2025-12-18 16:31:09 +0800

    3652 presum method

--- medium/3652/main.c
+++ medium/3652/main.c
@@ -1,34 +1,26 @@
+// presum method
 long long maxProfit(int* prices, int pricesSize, int* strategy, int strategySize, int k) {
   long long max = -1e18; // -1e18 is double percision
-  long long sum = 0;
-  long long base = 0;
-  long long mem;
+  long long *profitSum = (long long *) calloc(pricesSize+1, sizeof(long long));
+  long long *pricesSum = (long long *) calloc(pricesSize+1, sizeof(long long));
 
-  for (int i = 0; i < pricesSize; i++) base += prices[i] * strategy[i]; 
-  if (max < base) max = base;
+  for (int i = 0; i < pricesSize; i++) {
+    profitSum[i+1] = profitSum[i] + prices[i] * strategy[i]; 
+    pricesSum[i+1] = pricesSum[i] + prices[i];
+  }
 
-  mem = base;
-  for (int i = 0; i+k <= pricesSize; i++) {
-    // the first half --> strategy to hold (0)
-    for (int j = i; j < i+k/2; j++) {
-      if (strategy[j] != 0) {
-        base -= prices[j] * strategy[j];
-      }
+  max = profitSum[pricesSize];
+
+
+  // {{leftProfit}, {consecutive k profit}, {rightProfit}}
+  // Total profit = leftProfit + consecutive k profit + rightProfit
+  for (int i = k-1; i < pricesSize; i++) {
+    long long leftProfit = profitSum[i-k+1];
+    long long consecutiveProfit = pricesSum[i+1] - pricesSum[i- k/2 +1];
+    long long rightProfit = profitSum[pricesSize] - profitSum[i+1];
+    if (max < leftProfit + consecutiveProfit + rightProfit) {
+      max = leftProfit + consecutiveProfit + rightProfit;
     }
-
-    // the last half --> strategy to sell (1)
-    for (int j = i+k/2; j < i+k; j++) {
-      if (strategy[j] == 0) {
-        base += prices[j]; 
-      } else if (strategy[j] == -1) {
-        base -= prices[j] * strategy[j];
-        base += prices[j]; 
-      }
-    }
-
-    if (max < base) max = base;
-    base = mem;
-
   }
 
   return max;
