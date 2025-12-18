50560a6c02dc4286a7d603e6826dca7815626b65
tzyrrxr <sjlin0827@outlook.com>
2025-12-18 14:37:54 +0800

    3652 slide window -> TLE

--- medium/3652/main.c
+++ medium/3652/main.c
@@ -2,10 +2,13 @@ long long maxProfit(int* prices, int pricesSize, int* strategy, int strategySize
   long long max = -1e18; // -1e18 is double percision
   long long sum = 0;
   long long base = 0;
+  long long mem;
 
   for (int i = 0; i < pricesSize; i++) base += prices[i] * strategy[i]; 
+  if (max < base) max = base;
 
-  for (int i = 0; i+k < pricesSize; i++) {
+  mem = base;
+  for (int i = 0; i+k <= pricesSize; i++) {
     // the first half --> strategy to hold (0)
     for (int j = i; j < i+k/2; j++) {
       if (strategy[j] != 0) {
@@ -24,6 +27,7 @@ long long maxProfit(int* prices, int pricesSize, int* strategy, int strategySize
     }
 
     if (max < base) max = base;
+    base = mem;
 
   }
 
