f79e932f855b39576887347b142d48f24c7cf17c
tzyrrxr <sjlin0827@outlook.com>
2025-12-25 13:09:21 +0800

    496 TODO: Stack method

--- easy/496_Next_Creater_Element/main.c
+++ easy/496_Next_Creater_Element/main.c
@@ -1,41 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-//
-// fail
-//
+int CMP (const void *a, const void *b) {
+  return *(int*)b - *(int*)a;
+}
 
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* nextGreaterElement(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
- int *reSortNum2 = (int*) malloc((10000 + 1) * sizeof(int));
- int *ret = (int*) malloc(nums1Size * sizeof(int));
+  int *res = (int*) malloc(nums1Size*sizeof(int));
+  int *hash = (int*) calloc(10001, sizeof(int));
+  int *sorted = (int*) malloc(nums2Size*sizeof(int));
 
- for(int i = 1; i < nums2Size; i++) {
-  if (nums2[i] > nums2[i-1]) {
-   reSortNum2[nums2[i-1]] = nums2[i];
-  } else {
-   reSortNum2[nums2[i-1]] = -1;
+  memset(res, -1, nums1Size*sizeof(int));
+  memcpy(sorted, nums2, nums2Size*sizeof(int));
+  qsort(sorted, nums2Size, sizeof(int), CMP);
+
+  for (int i = 0; i < nums2Size; i++) {
+    hash[nums2[i]] = i;
   }
- }
- reSortNum2[nums2[nums2Size-1]] = -1;
 
- for (int i = 0; i < nums1Size; i++) {
-  ret[i] = reSortNum2[nums1[i]];
- }
+  for (int i = 0; i < nums1Size; i++) {
+    int elementIndex = hash[nums1[i]];
+    for (int j = 0, nearest = INT_MAX; sorted[j] > nums1[i]; j++) {
+      if (hash[sorted[j]] > elementIndex && hash[sorted[j]] - elementIndex < nearest) {
+        res[i] = sorted[j];
+        nearest = hash[sorted[j]] - elementIndex;
+      }
+    }
+  }
 
- *returnSize = nums1Size;
- free(reSortNum2);
+  free(hash);
+  free(sorted);
 
- return ret;
-}
-
-int main (int argc, char *argv[]) {
- int n1[] = {4, 1, 2};
- int n2[] = {1, 3, 4, 2};
- int n1Size;
- nextGreaterElement(n1, 3, n2, 4, &n1Size);
-
- return 0;
+  *returnSize = nums1Size;
+  return res;
+    
 }
