2806c459ce643ad9808077120b2dbb0107ec35e8
tzyrrxr <sjlin0827@outlook.com>
2025-12-27 17:08:05 +0800

    2483 Penalty

--- medium/2483/main.c
+++ medium/2483/main.c
@@ -1,18 +1,40 @@
 // brute force
 int bestClosingTime(char* customers) {
-  int res = 0;
+  int res;
+  int min = INT_MAX;
   int len = strlen (customers);
   int cntN = 0;
   int cntY = 0;
-  int leftPenalty = 0;
-  int rightPenalty = 0;
+  int allDay = 0;
+  int closedDay = 0;
 
   for (int i = 0; i < len; i++) {
     if (customers[i] == 'Y') cntY++;
     else cntN++;
   }
+  allDay = cntN;
+  closedDay = cntY;
 
-  for (int i = 0; i < len; i++)
+  if (min > allDay) { 
+   min = allDay;
+   res = len;
+  }
+
+  for (int i = len-1, curN = cntN, curY = cntY, preBest = 0, curPenalty = 0; i >= 0; i--) {
+    if (customers[i] == 'Y') {
+      curY--;
+    } else {
+      curN--;
+    }
+    curPenalty = curN + (cntY - curY);
+    preBest = curN;
+    if (curPenalty <= min) {
+      min = curPenalty;
+      res = i;
+    }
+  }
+
+  if (min > closedDay) res = 0;
 
   return res;
     
