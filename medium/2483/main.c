a95af8839939865aa247b87352d44730d63922f2
tzyrrxr <sjlin0827@outlook.com>
2025-12-27 18:16:33 +0800

    2483 two pass

--- medium/2483/main.c
+++ medium/2483/main.c
@@ -1,40 +1,28 @@
-// brute force
 int bestClosingTime(char* customers) {
   int res;
   int min = INT_MAX;
   int len = strlen (customers);
-  int cntN = 0;
-  int cntY = 0;
-  int allDay = 0;
-  int closedDay = 0;
+  int Penalty = 0;
 
   for (int i = 0; i < len; i++) {
-    if (customers[i] == 'Y') cntY++;
-    else cntN++;
-  }
-  allDay = cntN;
-  closedDay = cntY;
-
-  if (min > allDay) { 
-   min = allDay;
-   res = len;
+    if (customers[i] == 'Y') Penalty++; // Closed day penalty
   }
 
-  for (int i = len-1, curN = cntN, curY = cntY, preBest = 0, curPenalty = 0; i >= 0; i--) {
+  min = Penalty;
+  res = 0;
+  for (int i = 0; i < len; i++) {
     if (customers[i] == 'Y') {
-      curY--;
+      Penalty--;
     } else {
-      curN--;
+      Penalty++;
     }
-    curPenalty = curN + (cntY - curY);
-    preBest = curN;
-    if (curPenalty <= min) {
-      min = curPenalty;
-      res = i;
-    }
-  }
 
-  if (min > closedDay) res = 0;
+    if (Penalty < min) {
+      min = Penalty;
+      res = i+1;
+    }
+
+  }
 
   return res;
     
