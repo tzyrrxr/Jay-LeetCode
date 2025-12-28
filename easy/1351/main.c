0f203d9543fe1b79d05dac9e292b2404e94ff342
tzyrrxr <sjlin0827@outlook.com>
2025-12-28 15:41:36 +0800

    1351 Linear Traversal

--- easy/1351/main.c
+++ easy/1351/main.c
@@ -1,10 +1,27 @@
+//        n
+//     <-----
+//   ^ B ...
+//   | .    .
+// m | .    .
+//   | .    .
+//   | ...  S
+//
+// 1. check [0][n], if negative, then all below are negative, then check [0][n-1] until positve found.
+// 2. if [0][n] is positive, check[1][n], until negative found.
+//
 int countNegatives(int** grid, int gridSize, int* gridColSize) {
   int cnt = 0;
   int bit = sizeof(int) * 8 - 1;;
+  int m = gridSize;
+  int n = *gridColSize;
+  int i = 0, j = n-1;
 
-  for (int i = 0; i < gridSize; i++) {
-    for (int j = 0; j < *gridColSize; j++) {
-      if (grid[i][j] >> bit) cnt++;
+  while (i < m && j >= 0) {
+    if (grid[i][j] >> bit) {
+      cnt += m - i;
+      j--;
+    } else {
+      i++;
     }
   }
     
