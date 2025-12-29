3bb11619671484035fc255b6e0ac0779511ae217
tzyrrxr <sjlin0827@outlook.com>
2025-12-29 22:33:56 +0800

    756 tmp DFS + Backtracking

---
+++ medium/756/main.c
@@ -0,0 +1,13 @@
+#include <stdbool.h>
+
+// DFS
+bool pyramidTransition(char* bottom, char** allowed, int allowedSize) {
+  int res = false;
+    
+  return res;
+}
+
+int main () {
+  
+  return 0;
+}
