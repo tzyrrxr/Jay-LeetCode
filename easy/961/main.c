2445a77e3e7bf66321664f71cf444edf0f17bb34
tzyrrxr <sjlin0827@outlook.com>
2026-01-02 15:07:21 +0800

    961 Compare method, TC: O(N); SC: O(1)

--- easy/961/main.c
+++ easy/961/main.c
@@ -1,12 +1,17 @@
+// Permutation problem
+// n+1 unique numbers
+// numsSize == 2n
+// How far is the repeated number between itself?
+// Think the color balls
+// n different colors balls and n balls with same color
+// The balls are placed each other. One color ball is placed between two same color balls.
+// 4 balls group together, there are 2 same color balls if we can grap a group each time.
 int repeatedNTimes(int* nums, int numsSize) {
-  int hash[10001] = {0};
-  for (int i = 0; i < numsSize; i++) {
-    if (hash[nums[i]] == 1) {
-      return nums[i];
+  for (int bar = 1; bar <= 3; bar++) {
+    for (int i = 0; i < numsSize - bar; i++) {
+      if (nums[i] == nums[i + bar]) return nums[i];
     }
-    hash[nums[i]]++;
   }
 
   return -1;
-
 }
