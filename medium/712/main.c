e6019a251598bc9cfc4d53d12a4900f3b3f18418
tzyrrxr <sjlin0827@outlook.com>
2026-01-10 15:37:20 +0800

    712 1. LCS py3 VER. 2. Simplify memoization logic

--- medium/712/main.c
+++ medium/712/main.c
@@ -7,7 +7,7 @@
 int minimumDeleteSum(char* s1, char* s2) {
   int len1 = strlen(s1);
   int len2 = strlen(s2);
-  int memo[len2+1][len1+1];
+  int memo[len1+1][len2+1];
   int totalSum = 0;
   for (int i = 0; i < len1; i++) totalSum += (int) s1[i];
   for (int i = 0; i < len2; i++) totalSum += (int) s2[i];
@@ -15,11 +15,11 @@ int minimumDeleteSum(char* s1, char* s2) {
   // initialize memoization table
   // the first column
   for (int i = 0; i <= len1; i++) {
-    memo[0][i] = 0;
+    memo[i][0] = 0;
   }
   // the first row
   for (int j = 0; j <= len2; j++) {
-    memo[j][0] = 0;
+    memo[0][j] = 0;
   }
 
   // fill the memoization table
@@ -28,14 +28,14 @@ int minimumDeleteSum(char* s1, char* s2) {
       // common character
       // because the memo table is 1 index ahead, we need to -1 to get the character from the string
       if (s1[i-1] == s2[j-1]) {
-        memo[j][i] = memo[j-1][i-1] + (int) s1[i-1]; // or plus s2[j-1], they are the sam
+        memo[i][j] = memo[i-1][j-1] + (int) s1[i-1]; // or plus s2[j-1], they are the sam
       } else {
         // not common character
-        memo[j][i] = memo[j-1][i] > memo[j][i-1] ? memo[j-1][i] : memo[j][i-1];
+        memo[i][j] = memo[i-1][j] > memo[i][j-1] ? memo[i-1][j] : memo[i][j-1];
       }
     }
   }
 
   // 2 times the LCS ASCII sum need to be subtracted from total sum
-  return totalSum - 2*memo[len2][len1];
+  return totalSum - 2*memo[len1][len2];
 }
--- medium/712/main.cpp
+++ medium/712/main.cpp
@@ -8,20 +8,20 @@ public:
     int minimumDeleteSum(string s1, string s2) {
       int m = s1.length();
       int n = s2.length();
-      vector<vector<int>> memo(n+1, vector<int> (m+1, 0));
+      vector<vector<int>> memo(m+1, vector<int> (n+1, 0));
       int totalSum = 0;
       for (char c : s1) totalSum += (int)c;
       for (char c : s2) totalSum += (int)c;
       for (int i = 1; i <=m; i++) {
         for (int j = 1; j <= n; j++) {
           if (s1[i-1] == s2[j-1]) {
-            memo[j][i] = memo[j-1][i-1] + (int)s1[i-1]; // or s2[j-1]
+            memo[i][j] = memo[i-1][j-1] + (int)s1[i-1]; // or s2[j-1]
           } else {
-            memo[j][i] = memo[j-1][i] > memo[j][i-1] ? memo[j-1][i] : memo[j][i-1];
+            memo[i][j] = memo[i-1][j] > memo[i][j-1] ? memo[i-1][j] : memo[i][j-1];
           }
         }
       }
         
-      return totalSum - 2 * memo[n][m];
+      return totalSum - 2 * memo[m][n];
     }
 };
