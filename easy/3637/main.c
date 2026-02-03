776e95378cc8892414d9104f1c436e097b9905cb
tzyrrxr <sjlin0827@outlook.com>
2026-02-03 11:16:22 +0800

    3637 by turning points

--- easy/3637/main.c
+++ easy/3637/main.c
@@ -1,4 +1,5 @@
 bool isTrionic(int* nums, int numsSize) {
+  /*
   if (numsSize <= 3) return false;
   bool checker = false;
   int i = 1;
@@ -21,5 +22,16 @@ bool isTrionic(int* nums, int numsSize) {
   }
     
   return i == numsSize ? true: false;
+  */
+
+  if (numsSize <= 3) return false;
+  if (nums[0] >= nums[1]) return false;
+  int cnt = 0;
+  for (int i = 2; i < numsSize; i++) {
+    if (nums[i] == nums[i-1]) return false;
+    if ((nums[i] - nums[i-1]) * (nums[i-1] - nums[i-2]) < 0) cnt++;
+  }
+  // Does it have two turning points?
+  return cnt == 2;
 
 }
