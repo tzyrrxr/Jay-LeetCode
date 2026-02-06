79658284f39834936cd622a7f8c33c15ec896841
tzyrrxr <sjlin0827@outlook.com>
2026-02-06 17:52:50 +0800

    3634 type hell

--- medium/3634/main.c
+++ medium/3634/main.c
@@ -1,24 +1,21 @@
-int DFS (int cnt, int s, int e, int *nums, int k) {
-  if (!(s<=e)) return 0;
-  if (nums[s]*k >= nums[e]) return cnt;
-  int l, r;
-  if (nums[s]*k < nums[e]) {
-    l = DFS(cnt+1, s+1, e, nums, k);
-    r = DFS(cnt+1, s, e-1, nums, k);
-  }
-  if (l != 0 && r != 0)
-    return l < r ? l : r;
-  else if (l == 0) return r;
-  else if (r == 0) return l;
-  
-  return 0;
-}
-
 int CMP (const void *a, const void *b) {
   return *(int*) a - *(int*)b;
 }
+
 int minRemoval(int* nums, int numsSize, int k) {
+  int s = 0, e = 0;
+  int maxRange = INT_MIN;
   qsort(nums, numsSize, sizeof(int), CMP);
-    
-  return DFS(0, 0, numsSize-1, nums, k);
+
+  for (e = 0; e < numsSize; e++) {
+     if (s <= e &&
+         (long long)nums[e] > (long long)nums[s]*k
+         ) {
+       s++;
+     }
+     int currRange = e - s + 1;
+     if (currRange > maxRange) maxRange = currRange;
+  }
+
+  return numsSize - maxRange;
 }
