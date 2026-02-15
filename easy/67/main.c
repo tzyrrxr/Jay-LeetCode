61e49425ab7cc1e3547a87e3230382ed9da30eec
tzyrrxr <sjlin0827@outlook.com>
2026-02-15 12:21:51 +0800

    67 Safely handle memory regions

--- easy/67/main.c
+++ easy/67/main.c
@@ -4,16 +4,23 @@ char* addBinary(char* a, char* b) {
   int ch_len = fmax(a_len, b_len)+1;
   bool carry = 0;
   char *ch = (char*) malloc((ch_len+1) * sizeof(char));
-  ch[ch_len] = '\0';
+  if (!ch) return NULL;
 
-  while (ch_len-- > 0) {
+  ch[ch_len] = '\0';
+  int curr_index = ch_len-1;
+
+  while (curr_index >= 0) {
     int a_bit = --a_len < 0 ? 0 : a[a_len] - '0';
     int b_bit = --b_len < 0 ? 0 : b[b_len] - '0';
     int sum = a_bit + b_bit + (int)carry;
-    ch[ch_len] = (sum & 1) + '0';
+    ch[curr_index--] = (sum & 1) + '0';
     carry = (bool)(sum >> 1);
 
   }
+  if (*ch == '0') {
+    memmove(ch, ch+1, ch_len*sizeof(char));
+    ch = realloc(ch, ch_len*sizeof(char));
+  }
     
-  return *ch == '0' ? ch+1 : ch;
+  return ch;
 }
