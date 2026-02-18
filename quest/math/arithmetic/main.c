52f50b3a64eb1444e849351302f3cb6f61a05304
tzyrrxr <sjlin0827@outlook.com>
2026-02-18 12:38:56 +0800

    math arithmetic DP

--- quest/math/arithmetic/main.c
+++ quest/math/arithmetic/main.c
@@ -1,22 +1,37 @@
 bool canMakeArithmeticProgression(int* arr, int arrSize) {
   int n = arrSize;
   int d = 0;
+  int max = INT_MIN;
+  int min = INT_MAX;
+  bool res = true;
 
   for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n-i-1; j++) {
-      if (arr[j] > arr[j+1]) {
-        int tmp = arr[j];
-        arr[j] = arr[j+1];
-        arr[j+1] = tmp;
-      }
+    if (max < arr[i]) max = arr[i];
+    if (min > arr[i]) min = arr[i];
+  }
+
+  if (max == min) return true;
+
+  d = (max-min) / (n-1);
+  if (d == 0) return false;
+
+  bool *visited = (bool*) calloc(n, sizeof(bool));
+  int index;
+  for (int i = 0; i < n; i++) {
+    if ((arr[i]-min) % d) {
+      res = false;
+      break;
     }
+    index = (arr[i]-min)/d;
+    if (index >= n || visited[index]) {
+      res = false;
+      break;
+    }
+    visited[index] = true;
   }
 
-  d = arr[0] - arr[1];
-  for (int i = 1; i < n-1; i++) {
-    if (arr[i] - arr[i+1] != d) return false;
-  }
+  free(visited);
 
-  return true;
+  return res;
     
 }
