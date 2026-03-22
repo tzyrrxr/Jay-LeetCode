0b9e492198fc21f9820efbeb192ad07a36786a5e
tzyrrxr <sjlin0827@outlook.com>
2026-03-22 09:40:45 +0800

    1886 bitwise version

--- easy/1886/main.c
+++ easy/1886/main.c
@@ -1,58 +1,17 @@
 bool findRotation(int** mat, int matSize, int* matColSize, int** target, int targetSize, int* targetColSize) {
-    int mm = matSize;
-    int mn = *matColSize;
+    int n = matSize;
+    int res = 0b1111;
     
-    int tm = targetSize;
-    int tn = *targetColSize;
-
-    bool res = true;
-    
-    // 0
-    for (int i = 0; i < mm && res == true; i++) {
-        for (int j = 0; j < mm; j++) {
-            if (target[i][j] != mat[i][j]) {
-                res = false;
-                break;
-            }
-        }
-    }
-    if (res == true) return res;
-    else res = true;
-    
-    // 90
-    for (int i = 0, mj = mn-1; i < mm && mj >= 0 && res == true; i++, mj--) {
-        for (int j = 0, mi = 0; j < mm, mi < mm; j++, mi++) {
-            if (target[i][j] != mat[mi][mj]) {
-                res = false;
-                break;
-            }
-        }
-    }
-    if (res == true) return res;
-    else res = true;
-
-    // 180
-    for (int i = 0, mi = mm-1; i < mm && mi >= 0 && res == true; i++, mi--) {
-        for (int j = 0, mj = mn-1; j < mm, mj >= 0; j++, mj--) {
-            if (target[i][j] != mat[mi][mj]) {
-                res = false;
-                break;
-            }
-        }
-    }
-    if (res == true) return res;
-    else res = true;
-
-    // 270
-    for (int i = 0, mj = 0; i < mm && mj < mn && res == true; i++, mj++) {
-        for (int j = 0, mi = mm-1; j < mm, mi >= 0; j++, mi--) {
-            if (target[i][j] != mat[mi][mj]) {
-                res = false;
-                break;
-            }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if ((res & 0b1000) && target[i][j] != mat[i][j]) res &= 0b0111;
+            if ((res & 0b0100) && target[i][j] != mat[j][n-i-1]) res &= 0b1011;
+            if ((res & 0b0010) && target[i][j] != mat[n-i-1][n-j-1]) res &= 0b1101;
+            if ((res & 0b0001) && target[i][j] != mat[n-j-1][i]) res &= 0b1110;
+            if (res == 0b0000) return false;
         }
     }
     
-    return res;
+    return res != 0;
 
 }
