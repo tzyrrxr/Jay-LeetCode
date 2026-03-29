49e7c38a1afaca70e34888a52e7ed0711f98135e
tzyrrxr <sjlin0827@outlook.com>
2026-03-29 09:42:22 +0800

    2839 hash version.

--- easy/2839/main.c
+++ easy/2839/main.c
@@ -1,8 +1,22 @@
 bool canBeEqual(char* s1, char* s2) {
   int len = strlen(s1);
+  int evenHash[26] = {0};
+  int oddHash[26] = {0};
 
-  for (int i = 0; i < len-2; i++) {
-    if (!((s1[i]^s2[i]) == 0 || (s1[i]^s2[i+2]) == 0) || !((s1[i+2]^s2[i]) == 0 || (s1[i+2]^s2[i+2]) == 0)) return false;
+  for (int i = 0; i < len; i++) {
+
+    if ((i%2) == 0) {
+      evenHash[s1[i] - 'a']++;
+      evenHash[s2[i] - 'a']--;
+    } else {
+      oddHash[s1[i] - 'a']++;
+      oddHash[s2[i] - 'a']--;
+    }
+
+  }
+
+  for (int i = 0; i < 26; i++) {
+    if (evenHash[i] != 0 || oddHash[i] != 0) return false;
   }
 
   return true;
