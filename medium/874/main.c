29b9d661d661a4660fbf0eabe24b700032c6540d
tzyrrxr <sjlin0827@outlook.com>
2026-04-06 16:38:22 +0800

    874 hash(macro), unsign bit; 3640 tmp

--- medium/874/main.c
+++ medium/874/main.c
@@ -1,4 +1,8 @@
-#define SIZE 30001
+typedef struct {
+  unsigned long long key;
+  UT_hash_handle hh;
+} Node;
+
 // brute force
 int robotSim(int* commands, int commandsSize, int** obstacles, int obstaclesSize, int* obstaclesColSize) {
  int dir[4][2] = {
@@ -7,14 +11,16 @@ int robotSim(int* commands, int commandsSize, int** obstacles, int obstaclesSize
   { 0,-1},
   {-1, 0},
  };
-
+ Node *users = NULL;
  int pt = 0;
  int pos[2] = {0,0};
  int max = 0;
- bool hash[SIZE][SIZE];
- memset(hash, 0, sizeof(bool)*(SIZE)*(SIZE));
  for (int k = 0; k < obstaclesSize; k++) {
-   hash[obstacles[k][0]+SIZE][obstacles[k][1]+SIZE] = 1;
+   int x = obstacles[k][0];
+   int y = obstacles[k][1];
+   Node *new = (Node *) malloc(sizeof(Node));
+   new->key = ((unsigned long long)x << 32) | (y & 0xffffffff);
+   HASH_ADD(hh, users, key, sizeof(long long),new);
  }
 
  for (int i = 0; i < commandsSize; i++) {
@@ -30,8 +36,11 @@ int robotSim(int* commands, int commandsSize, int** obstacles, int obstaclesSize
        for (int j = 0; j < commands[i]; j++) {
          int x =  pos[0] + dir[pt][0];
          int y =  pos[1] + dir[pt][1];
+         unsigned long long target = ((unsigned long long)x << 32) | (y & 0xffffffff);
+         Node *hit = NULL;
+         HASH_FIND(hh, users, &target, sizeof(long long), hit);
 
-         if (hash[x+SIZE][y+SIZE]) {
+         if (hit) {
            break;
          } else {
            pos[0] = x;
