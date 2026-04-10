019f06f1eabe19c80a2c52c3a1a5cbebd425cce7
tzyrrxr <sjlin0827@outlook.com>
2026-04-11 07:31:10 +0800

    3740

--- easy/3740/main.c
+++ easy/3740/main.c
@@ -2,16 +2,26 @@ int minimumDistance(int* nums, int numsSize) {
     int n = numsSize;
     if (n < 3) return -1;
     int min = INT_MAX;
+    int hash[101] = {0};
+
+    for (int i = 0; i < n; i++) {
+        hash[nums[i]]++;
+    }
     
     for (int i = 0; i < n; i++) {
-        for (int j = i+1; j < n; j++) {
-            if (nums[i] != nums[j]) continue;
-            for (int k = j+1; k < n; k++) {
-                if (nums[j] != nums[k]) continue;
-                min = fmin(min, k-i + k-j + j-i);
-                j = n;
-                break;
+        if (hash[nums[i]] > 2) {
+            int target = nums[i];
+            int pick = nums[i];
+            for (int j = i+1; j<n; j++) {
+                if (target == nums[j]) {
+                    pick ^= target;
+                    if (pick == target) {
+                        min = fmin(min, (j-i)*2);
+                        break;
+                    }
+                }
             }
+            hash[nums[i]]--;
         }
     }
     
