c583637e3efdc155fc335ecb741103477a070406
tzyrrxr <sjlin0827@outlook.com>
2026-05-11 20:25:49 +0800

    2553, can not free

---
+++ easy/2553/main.c
@@ -0,0 +1,26 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* separateDigits(int* nums, int numsSize, int* returnSize) {
+    int n = numsSize;
+    int max = 6 * n;
+    int *ret = (int *) malloc(max*sizeof(int));
+    int pt = max-1;
+
+    for (int i = n-1; i >= 0; i--) {
+        int num = nums[i];
+        if (num == 0) {
+            ret[pt--] = 0;
+        } else {
+            while(num > 0) {
+                ret[pt--] = num%10;
+                num /= 10;
+            }
+        }
+    }
+
+    *returnSize = max -1 - pt;
+
+    return ret + pt + 1;
+    
+}
