1654f89f2e46046bc266c115c5ac696b3be62b18
tzyrrxr <sjlin0827@outlook.com>
2026-05-23 10:50:41 +0800

    1752

--- easy/1752/main.c
+++ easy/1752/main.c
@@ -1,24 +1,13 @@
 bool check(int* nums, int numsSize) {
-    int *tmp = (int*) malloc(numsSize*sizeof(int));
-    bool match = true;
+    int critical = 0;
 
-    memcpy(tmp, nums, numsSize*sizeof(int));
-    qsort(tmp, numsSize, sizeof(int), CMP);
-
-    for (int offset = 0; offset < numsSize; offset++) {
-        match = true;
-        for (int i = 0; i < numsSize; i++) {
-            if (nums[(offset + i)%numsSize] != tmp[i]) {
-                match = false;
-                break;
-            }
-        }
-        if (match) {
-            return true;
+    for (int i = 0; i < numsSize; i++) {
+        if(nums[i] > nums[(i+1)%numsSize]) {
+            critical++;
         }
     }
 
-    free(tmp);
-    
-    return false;
+    if (critical > 1) return false;
+
+    return true;
 }
