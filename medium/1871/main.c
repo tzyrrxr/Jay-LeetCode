898278da10508284725c95bc7710a903c9ddfae7
tzyrrxr <sjlin0827@outlook.com>
2026-05-25 21:47:44 +0800

    1871 sliding window

--- medium/1871/main.c
+++ medium/1871/main.c
@@ -1,32 +1,31 @@
 bool canReach(char* s, int minJump, int maxJump) {
   int len = strlen(s);
   if (s[len-1] == '1') return false;
+
   bool ret;
   bool *reach = (bool*) calloc(len, sizeof(bool));
-  int *ReachCnt = (int*) malloc(len*sizeof(int)); // prefix sum
+  reach[0] = true;
 
-  for (int i = 0; i < minJump; i++) {
-    ReachCnt[i] = 1;
-  }
+  int count = 0;
 
-  for (int i = minJump; i < len; i++) {
-    // jump range to i position
+  for (int i = 1; i < len; i++) {
     int l = i - maxJump;
     int r = i - minJump;
-    if ('0' == s[i]) {
-      int count = ReachCnt[r];
-      if (0 <= l-1) {
-        count -= ReachCnt[l-1];
+    if (0 <= r) {
+      if (reach[r]) {
+        count++;
       }
-      if (count != 0) {
-        reach[i] = true; // The index i can be reach.
+      if (0 <= l-1 && reach[l-1]) {
+        count--;
       }
     }
-    ReachCnt[i] = ReachCnt[i-1] + reach[i]; // The default value of reach[i] is zero.
+    if(count && '0' == s[i]) {
+      reach[i] = true;
+    }
   }
+
   ret = (true == reach[len-1]);
   free(reach);
-  free(ReachCnt);
     
   return ret;
 }
